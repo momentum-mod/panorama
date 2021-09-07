847a9d21d143bbb88dd73ece24eea128259fd990
brae <brae@momentum-mod.org>
2021-09-07 16:20:31 -0700

    feat: conc entity panels

---
+++ scripts/hud/concentities.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class ConcEntities {
+	static entPanelContainer = $( '#ConcEntPanelsContainer' );
+	static onEntPanelThink() {
+		ConcEntities.entPanelContainer.Children().filter( entpanel => entpanel.HasClass( 'conc-ent-panel__cont' ) ).forEach( entpanel => {
+			let meterEnabled = $.GetContextPanel().concEntPanelProgressBarEnabled;
+			let meter = entpanel.FindChildTraverse( 'ConcTimeMeter' );
+			meter.visible = meterEnabled;
+			if ( meterEnabled )
+				meter.value = entpanel.concPrimedPercent;
+
+			let labelEnabled = $.GetContextPanel().concEntPanelTimerLabelEnabled;
+			let label = entpanel.FindChildTraverse( 'ConcTimeLabel' );
+			label.visible = labelEnabled;
+			if ( labelEnabled )
+				label.text = `${ entpanel.concPrimedTime.toFixed( 2 ) }s`;
+
+			entpanel.style.opacity = entpanel.concDistanceFadeAlpha;
+		} );
+	}
+}
+
+( function() {
+	$.RegisterEventHandler( 'OnConcEntityPanelThink', $( '#ConcEntPanelsContainer' ), ConcEntities.onEntPanelThink );
+})();
