b2ac6dac6134185c009a3b2f2589ddde030372db
brae <brae@momentum-mod.org>
2021-09-19 19:15:47 -0700

    feat: ghost entpanels

---
+++ scripts/hud/ghostentities.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class GhostEntities {
+	static onAimOverGhostChange( entpanel, aimover ) {
+		let nameEnabled = $.GetContextPanel().ghostNamesEnabled && aimover;
+		let namePanel = entpanel.FindChildTraverse( 'NamePanel' );
+		namePanel.SetHasClass( 'namepanel--hidden', !nameEnabled );
+
+		// always keep centered even when name isnt visible
+		if ( nameEnabled )
+		{
+			entpanel.style.transform = 'translatex( 0 )';
+		}
+		else
+		{
+			let nameLabel = namePanel.FindChildTraverse( 'GhostName' );
+			entpanel.style.transform = `translatex( ${ nameLabel.actuallayoutwidth / nameLabel.actualuiscale_x / 2 }px )`;
+		}
+	}
+}
+
+( function() {
+	$.RegisterEventHandler( 'OnAimOverGhostChange', $( '#GhostEntPanelsContainer' ), GhostEntities.onAimOverGhostChange )
+} )();
