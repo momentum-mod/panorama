641d131a69d4418f13e7ede5854ccd3ed1548ae9
Leon <leonlvaness@gmail.com>
2021-10-24 00:46:14 +0200

    feat(panorama): Add popup for color picker

---
+++ scripts/popup/colorpicker.js
@@ -0,0 +1,21 @@
+class ColorPickerPopup {
+
+	static init() {
+		let color = $.GetContextPanel().GetAttributeString('color', 'rgba(0,0,0,255)');
+		$('#ColorPicker').prevColor = color;
+		$('#ColorPicker').currColor = color;
+	}
+
+	static onSaveColor(color) {
+		UiToolkitAPI.CloseAllVisiblePopups();
+	}
+
+	static onDiscardColor() {
+		UiToolkitAPI.CloseAllVisiblePopups();
+	}
+
+	static {
+		$.RegisterEventHandler('ColorPickerSave', $.GetContextPanel(), ColorPickerPopup.onSaveColor);
+		$.RegisterEventHandler('ColorPickerCancel', $.GetContextPanel(),ColorPickerPopup.onDiscardColor);
+	}
+}
