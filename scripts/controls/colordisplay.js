1c1a17c7519e413af147d4c4015f1b9eef468076
brae <brae@momentum-mod.org>
2021-11-13 02:00:19 -0800

    refactor: convarcolordisplay -> colordisplay
    Have the ability to use just ColorDisplay for a version that doesn't have a convar tied to it. ConVarColorDisplay inherits from ColorDisplay.

--- scripts/controls/colordisplay.js
+++ scripts/controls/colordisplay.js
@@ -1,8 +1,10 @@
-class ConVarColorDisplay {
+'use_strict';
+
+class ColorDisplay {
 	static showPopup() {
 		let color = $.GetContextPanel().color;
 		let popup = UiToolkitAPI.ShowCustomLayoutPopupParameters("", "file://{resources}/layout/popups/popup_colorpicker.xml", "color=" + color);
-		$.RegisterEventHandler('ColorPickerSave', popup, ConVarColorDisplay.saveColor);
+		$.RegisterEventHandler('ColorPickerSave', popup, ColorDisplay.saveColor);
 	}
 
 	static saveColor(color) {
