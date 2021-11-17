6d3339ad46fbf00ebe38f9be7e3f6a7e0b7b7599
brae <brae@momentum-mod.org>
2021-11-17 04:56:27 -0800

    feat: range color name popup
    to input name for a new color profile or change the name of an existing one

---
+++ scripts/popup/rangecolorprofilename.js
@@ -0,0 +1,48 @@
+'use_strict';
+
+class RangeColorProfileNamePopup {
+	static textEntry = $('#RangeColorProfileName');
+	static invalidNameLabel = $('#InvalidNameLabel');
+	static profileNames = [];
+
+	static onTextSubmitted() {
+		const text = this.textEntry.text;
+		if (text === '' || text === 'No Profile' || text.includes(',')) {
+			RangeColorProfileNamePopup.invalidNameSubmitted();
+			return;
+		}
+
+		let passed = true;
+		RangeColorProfileNamePopup.profileNames.every(profileName => {
+			if (text === profileName) {
+				passed = false;
+				return false;
+			}
+			return true;
+		});
+		if (!passed) {
+			RangeColorProfileNamePopup.invalidNameSubmitted();
+			return;
+		}
+
+		const callbackHandle = $.GetContextPanel().GetAttributeInt('callback', -1);
+		if ( callbackHandle !== -1 )
+			UiToolkitAPI.InvokeJSCallback( callbackHandle, text );
+		UiToolkitAPI.CloseAllVisiblePopups();
+	}
+
+	static invalidNameSubmitted() {
+		RangeColorProfileNamePopup.invalidNameLabel.visible = true;
+	}
+
+	static onOKPressed() {
+		this.textEntry.Submit();
+	}
+
+	static init() {
+		RangeColorProfileNamePopup.invalidNameLabel.visible = false;
+		$.GetContextPanel().SetDialogVariable('OKBtnText', $.GetContextPanel().GetAttributeString('OKBtnText', ''));
+		RangeColorProfileNamePopup.profileNames = $.GetContextPanel().GetAttributeString('profileNames', '').split(',');
+		this.textEntry.text = $.GetContextPanel().GetAttributeString('prefilledText', '');
+	}
+}
