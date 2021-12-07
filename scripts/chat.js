6a1505e9bf9cc7cc38ca8a9c8220be5e0a661487
tsa96 <tom@trumpetdust.org>
2021-12-07 20:45:20 +0000

    feat: Improve chat visibility
    Removes a useless old class, small styling things, cleans up the JS

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,28 +1,40 @@
-"use strict";
+'use strict';
 
 class Chat {
 	static arrMembersTyping = [];
-	static typingLabel;
+	static typingLabel = $('#ChatMemberTypingLabel');
+
+	static {
+		$.RegisterEventHandler('OnNewChatEntry', $.GetContextPanel(), Chat.onNewChatEntry);
+		$.RegisterForUnhandledEvent('PanoramaComponent_SteamLobby_OnMemberDataUpdated', Chat.onSteamLobbyMemberDataUpdated);
+	}
 
 	static submitText() {
 		$.GetContextPanel().SubmitText();
 	}
 
 	static createUsersTypingString(users) {
-		let strTyping = "";
+		let strTyping = '';
 		let typingLen = users.length;
 
 		// Just some simplified logic for the time being. TODO: do it properly
 		if (typingLen > 0) {
 			if (typingLen < 3) {
 				Chat.arrMembersTyping.forEach(function (memberSteamID, i) {
-					if (i !== 0 && i !== typingLen - 1) strTyping += ", ";
-					else if (i !== 0 && i === typingLen - 1) strTyping += " and ";
+					if (i !== 0 && i !== typingLen - 1) {
+						strTyping += ', ';
+					} else if (i !== 0 && i === typingLen - 1) {
+						strTyping += ' and ';
+					}
 					strTyping += FriendsAPI.GetNameForXUID(memberSteamID);
 				});
-				strTyping += typingLen === 1 ? " is typing" : " are typing";
-			} else strTyping += typingLen + " people are typing";
-		} else strTyping += " ";
+				strTyping += typingLen === 1 ? ' is typing' : ' are typing';
+			} else {
+				strTyping += typingLen + ' people are typing';
+			}
+		} else {
+			strTyping += ' ';
+		}
 
 		return strTyping;
 	}
@@ -35,6 +47,20 @@ class Chat {
 		// message = message.replace(/dogathan/g, "<img src='file://{images}/emotes/dogathan.png' height='16' width='16'/>");
 		// message = message.replace(/a/g, 'ä').replace(/o/g, 'ö').replace(/u/g, 'ü');
 		// messageLabel.text = message;
+
+		// TODO: Would be good to let you mute players from the specific chat message esp in large lobbies
+		// where it's hard to find them in the lobby list, but we need the steam id passed in somewhere
+		
+		// panel.SetPanelEvent('oncontextmenu', () => {
+		// 	UiToolkitAPI.ShowSimpleContextMenu(panel, '', [
+		// 		{
+		// 			label: 'Mute Player',
+		// 			icon: 'file://{images}/volume-mute.svg',
+		// 			style: 'icon-color-red',
+		// 			jsCallback: () => ChatAPI.ChangeMuteState(needs steam id!!!, true)
+		// 		}
+		// 	]);
+		// });
 	}
 
 	static onSteamLobbyMemberDataUpdated(data) {
@@ -47,7 +73,7 @@ class Chat {
 
 			const index = Chat.arrMembersTyping.indexOf(memberSteamID);
 
-			if (index === -1 && data[memberSteamID]["isTyping"] === "y") {
+			if (index === -1 && data[memberSteamID]['isTyping'] === 'y') {
 				Chat.arrMembersTyping.push(memberSteamID);
 			} else if (index !== -1) {
 				Chat.arrMembersTyping.splice(index, 1);
@@ -57,10 +83,3 @@ class Chat {
 		Chat.typingLabel.text = Chat.createUsersTypingString(Chat.arrMembersTyping);
 	}
 }
-
-(function () {
-	$.RegisterEventHandler("OnNewChatEntry", $.GetContextPanel(), Chat.onNewChatEntry);
-	$.RegisterForUnhandledEvent("PanoramaComponent_SteamLobby_OnMemberDataUpdated", Chat.onSteamLobbyMemberDataUpdated);
-
-	Chat.typingLabel = $.GetContextPanel().FindChildTraverse("ChatMemberTypingLabel");
-})();
