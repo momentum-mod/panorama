49eb36d6ed30e51083fb9f2ce1790e4bb7502da8
tsa96 <tom@trumpetdust.org>
2022-05-07 09:11:05 +0100

    feat: Add profile drawer page
    Very unfinished. Bunch of styled by unfinished stuff is on the feat/profile branch to work on in the future.

---
+++ scripts/context_menus/levelsexplainer.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class LevelsExplainer {
+	static fixLevelIndicators() {
+		// When these panels are first created, for some godforsaken reason they don't have the correct class until you
+		// mouse over them... I have absolutely no idea why, this is a quick fix.
+		const className = 'levels-explainer__level-indicator';
+		$.GetContextPanel()
+			.FindChildrenWithClassTraverse(className)
+			.forEach((p) => p.TriggerClass(className));
+	}
+}
