3fb8fb1d359291379aef66e84e7e13de366c991e
tsa96 <tom@trumpetdust.org>
2022-05-13 12:26:24 +0100

    feat: Intro Movie Cleanup

--- scripts/intromovie.js
+++ scripts/intromovie.js
@@ -1,14 +1,11 @@
 'use strict';
 
 class IntroMovie {
+	static {
+		$.RegisterForUnhandledEvent('ChaosShowIntroMovie', this.showIntroMovie.bind(this));
+	}
+
 	static showIntroMovie() {
 		$.DispatchEvent('ChaosHideIntroMovie'); // Disable intro movie for Momentum
 	}
 }
-
-//--------------------------------------------------------------------------------------------------
-// Entry point called when panel is created
-//--------------------------------------------------------------------------------------------------
-(function () {
-	$.RegisterForUnhandledEvent('ChaosShowIntroMovie', IntroMovie.showIntroMovie);
-})();
