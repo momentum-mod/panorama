9a945b844f4c9303dd4ea874340815d3930e3000
PeenScreeker <bumperjumper569@gmail.com>
2022-06-20 22:01:13 -0400

    feat: Add synchronizer bhop trainer
    adds bhop trainer with 4 visualization modes and settings entries for feedback customization
    
    fixup

--- scripts/common/settings.js
+++ scripts/common/settings.js
@@ -61,6 +61,7 @@ const SETTINGS_TABS = {
 			KeypressSubSection: 'KeypressRadio',
 			WeaponSelSubSection: 'WeaponSelRadio',
 			StrafeSyncSubSection: 'StrafeSyncRadio',
+			SynchronizerSubSection: 'SynchronizerRadio',
 			MapInfoSubSection: 'MapInfoRadio'
 		}
 	},
