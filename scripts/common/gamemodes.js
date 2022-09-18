03ac2c0e931212868edfd61a6b2f44b0d0bd22d3
tsa96 <tom@trumpetdust.org>
2022-09-18 10:12:06 +0100

    feat: Tokenize gamemode strings

--- scripts/common/gamemodes.js
+++ scripts/common/gamemodes.js
@@ -2,61 +2,61 @@
 
 const GAMEMODE = {
 	1: {
-		name: 'Surf',
-		shortName: 'Surf',
+		name: '#Gamemode_Surf',
+		shortName: '#Gamemode_Surf_Short',
 		prefix: 'surf_'
 	},
 	2: {
-		name: 'Bhop',
-		shortName: 'Bhop',
+		name: '#Gamemode_Bhop',
+		shortName: '#Gamemode_Bhop_Short',
 		prefix: 'bhop_'
 	},
 	3: {
-		name: 'Climb',
-		shortName: 'Climb',
+		name: '#Gamemode_Climb',
+		shortName: '#Gamemode_Climb_Short',
 		prefix: 'climb_'
 	},
 	4: {
-		name: 'Rocket Jump',
-		shortName: 'RJ',
+		name: '#Gamemode_RJ',
+		shortName: '#Gamemode_RJ_Short',
 		prefix: 'rj_'
 	},
 	5: {
-		name: 'Sticky Jump',
-		shortName: 'SJ',
+		name: '#Gamemode_SJ',
+		shortName: '#Gamemode_SJ_Short',
 		prefix: 'sj_'
 	},
 	6: {
-		name: 'Tricksurf',
-		shortName: 'Tricksurf',
+		name: '#Gamemode_Tricksurf',
+		shortName: '#Gamemode_Tricksurf_Short',
 		prefix: 'tsurf_'
 	},
 	7: {
-		name: 'Accelerated Hop',
-		shortName: 'Ahop',
+		name: '#Gamemode_Ahop',
+		shortName: '#Gamemode_Ahop_Short',
 		prefix: 'ahop_'
 	},
 	8: {
-		name: 'Parkour',
-		shortName: 'Parkour',
+		name: '#Gamemode_Parkour',
+		shortName: '#Gamemode_Parkour_Short',
 		prefix: 'pk_'
 	},
 	9: {
-		name: 'Conc',
-		shortName: 'Conc',
+		name: '#Gamemode_Conc',
+		shortName: '#Gamemode_Conc_Short',
 		prefix: 'conc_'
 	},
 	10: {
-		name: 'Defrag',
-		shortName: 'Defrag',
+		name: '#Gamemode_Defrag',
+		shortName: '#Gamemode_Defrag_Short',
 		prefix: 'df_'
 	}
 };
 
 const GAMEMODE_WITH_NULL = {
 	0: {
-		name: 'Unknown',
-		shortName: 'Unknown',
+		name: '#Gamemode_Unknown',
+		shortName: '#Unknown',
 		prefix: ''
 	},
 	...GAMEMODE
