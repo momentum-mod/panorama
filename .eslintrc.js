f1378359618a5e98bc640df1da946e375a0f91e1
tsa96 <tom@trumpetdust.org>
2023-01-21 00:31:03 +0000

    feat: Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	plugins: ['unicorn', 'prettier'],
+	extends: ['eslint:recommended', 'plugin:unicorn/recommended', 'prettier'],
+	root: true,
+	env: {
+		es2022: true
+	},
+	ignorePatterns: ['.eslintrc.js', 'node_modules', '__types_momentum.js'],
+	rules: {
+		'prefer-const': ['error'],
+		'class-methods-use-this': ['error'],
+		camelcase: ['warn'],
+		eqeqeq: ['error', 'smart'],
+		// Terrible for us, gets confused by $. and stuff defined in other files that are in same context
+		// due being in the same <imports> block.
+		'no-undef': ['off'],
+		'no-unused-vars': ['off'],
+		// I'd love to use this but too annoying a refactor, some C++ APIs look like they use `null`.
+		'unicorn/no-null': ['off'],
+		// Way too sensitive. Most cases it catches are silly, and bad naming is easy to flag in review.
+		'unicorn/prevent-abbreviations': ['off'],
+		// Not going to make devs use obscure JS syntax for something so minor.
+		'unicorn/numeric-separators-style': ['warn', { onlyIfContainsSeparator: true }],
+		// We don't use modules.
+		'unicorn/prefer-module': ['off'],
+		// This will hit for async IIFEs and warn about module crap. We don't use modules.
+		'unicorn/prefer-top-level-await': ['off'],
+		// Even though unicorn has a rule for no nested ternaries, it's insisting I make them due to this rule.
+		'unicorn/prefer-ternary': ['error', 'only-single-line'],
+		// Better parity with other langauges, we use `1 << 0` frequently next to other shifts when defining bitflags.
+		'unicorn/prefer-math-trunc': ['off'],
+		// Why???
+		'unicorn/switch-case-braces': ['off'],
+		// We like using static classes since we can use the ES2022 static initalisation blocks.
+		// This rule doesn't apply to classes with those blocks, but it's just annoying to differentiate
+		// the cases with and without static ctors. May as well just use everywhere, even if the JS nerds are
+		// correct that a static only class (without static ctor) can just be an Object.
+		'unicorn/no-static-only-class': ['off'],
+		// Who cares
+		'unicorn/filename-case': ['off'],
+		// Fuck you I wanna
+		'unicorn/no-abusive-eslint-disable': ['off'],
+		// Terrible rule, it's often practically useful to explicitly state what's happening in cases
+		'unicorn/no-useless-switch-case': ['off']
+	}
+};
