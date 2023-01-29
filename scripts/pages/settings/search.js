94d65af50c29fc4f4518dc7f35192a2ab40e9dca
tsa96 <tom@trumpetdust.org>
2023-01-29 11:05:05 +0000

    refactor: Use new Object.hasOwn over Object.prototype.hasOwnProperty
    Neat little addition in ES2022

--- scripts/pages/settings/search.js
+++ scripts/pages/settings/search.js
@@ -174,7 +174,7 @@ class SettingsSearch {
 	// Only check text on panels that have a text property, ignore dropdowns, headers, keybinder keys, radiobutton text
 	static shouldSearchPanelText(panel) {
 		return (
-			Object.prototype.hasOwnProperty.call(panel, 'text') &&
+			Object.hasOwn(panel, 'text') &&
 			panel.paneltype !== 'TextEntry' &&
 			panel.GetParent().paneltype !== 'Button' &&
 			!panel.HasClass('DropDownChild') &&
