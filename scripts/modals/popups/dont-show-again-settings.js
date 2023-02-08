a2ebb3b87e03e4dfdeb0456474bfc18797475d0f
tsa96 <tom@trumpetdust.org>
2023-02-08 07:51:20 +0000

    feat: Add popup for reseting DoSA items to settings

---
+++ scripts/modals/popups/dont-show-again-settings.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class DosaSettings {
+	static onLoad() {
+		const container = $('#Dosas');
+		for (const [id, nameToken] of DosaHandler.getAll()) {
+			const panel = $.CreatePanel('Panel', container, '');
+			panel.LoadLayoutSnippet('dosa-item');
+			panel.SetDialogVariable('name', $.Localize(nameToken));
+			panel.FindChild('ResetButton').SetPanelEvent('onactivate', () => {
+				DosaHandler.removeDosa(id);
+				panel.RemoveAndDeleteChildren();
+			});
+		}
+	}
+}
