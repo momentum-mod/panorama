bed4c9c0c674b5b1abb813938550384045a61574
brae <brae@momentum-mod.org>
2023-03-26 11:45:42 -0600

    feat: new hud speedometer
    On the new data format

--- scripts/common/speedometer.js
+++ scripts/common/speedometer.js
@@ -1,41 +1,38 @@
 'use strict';
 
-const SpeedometerUnitsType = {
-	UPS: 0,
-	KMH: 1,
-	MPH: 2
+const SpeedometerTypes = {
+	OVERALL_VELOCITY: 0,
+	EXPLOSION_VELOCITY: 1,
+	JUMP_VELOCITY: 2,
+	RAMP_VELOCITY: 3,
+	ZONE_VELOCITY: 4
 };
 
-const SpeedometerColorMode = {
+const SpeedometerDataKeys = {
+	CUSTOM_LABEL: 'custom_label',
+	TYPE: 'type',
+	ENABLED_AXES: 'enabled_axes',
+	COLOR_TYPE: 'color_type',
+	RANGE_COL_PROF: 'range_color_profile'
+};
+
+const RangeColorProfileKeys = {
+	PROFILE_NAME: 'profile_name',
+	PROFILE_RANGE_DATA: 'profile_ranges'
+};
+
+const SpeedometerColorTypes = {
 	NONE: 0,
 	RANGE: 1,
 	COMPARISON: 2,
 	COMPARISON_SEP: 3
 };
 
-// these key names match the names of speedometers in the .vdf files
-const SpeedometerIDs = {
-	AbsSpeedometer: 0,
-	HorizSpeedometer: 1,
-	VertSpeedometer: 2,
-	EnergySpeedometer: 3,
-	ExplosiveJumpVelocity: 4,
-	LastJumpVelocity: 5,
-	RampBoardVelocity: 6,
-	RampLeaveVelocity: 7,
-	StageEnterExitAbsVelocity: 8,
-	StageEnterExitHorizVelocity: 9
-};
-
+// To be index via SpeedometerTypes
 const SpeedometerDispNames = [
-	'#Speedometer_Type_Absolute',
-	'#Speedometer_Type_Horizonal',
-	'#Speedometer_Type_Vertical',
-	'#Speedometer_Type_Energy',
+	'#Speedometer_Type_OverallVelocity',
 	'#Speedometer_Type_ExplosiveJump',
 	'#Speedometer_Type_Jump',
-	'#Speedometer_Type_RampBoard',
-	'#Speedometer_Type_RampLeave',
-	'#Speedometer_Type_StageAbsolute',
-	'#Speedometer_Type_StageHorizontal'
+	'#Speedometer_Type_Ramp',
+	'#Speedometer_Type_Zone'
 ];
