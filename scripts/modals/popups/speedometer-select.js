7cbe54c0c8e2385ff97f86026b15da79c5da7146
Kyoshi7 <quackthebhopper@gmail.com>
2023-12-08 23:12:21 -0500

    fix: check for invalid speedometer names

--- scripts/modals/popups/speedometer-select.js
+++ scripts/modals/popups/speedometer-select.js
@@ -3,20 +3,46 @@ class SpeedometerSelectPopup {
 	static container = $('#SpeedometerSelectContainer');
 	/** @static @type {TextEntry} */
 	static textEntry = $('#SpeedometerName');
+	/** @type {Label} @static */
+	static invalidNameLabel = $('#InvalidNameLabel');
 	static selected = 0;
+	static speedometerNames = [];
 
-	static onAddButtonPressed() {
+	static onTextSubmitted() {
+		if (!this.validateSpeedometerNames()) {
+			this.invalidNameLabel.visible = true;
+			return;
+		}
+
 		const callbackHandle = $.GetContextPanel().GetAttributeInt('callback', -1);
+
 		if (callbackHandle !== -1)
-			UiToolkitAPI.InvokeJSCallback(
-				callbackHandle,
-				SpeedometerSelectPopup.selected,
-				SpeedometerSelectPopup.textEntry.text
-			);
+			UiToolkitAPI.InvokeJSCallback(callbackHandle, SpeedometerSelectPopup.selected, this.textEntry.text);
 		UiToolkitAPI.CloseAllVisiblePopups();
 	}
 
+	static validateSpeedometerNames() {
+		const text = this.textEntry.text;
+		if (text === '' || text.includes(',')) return false;
+
+		if (this.speedometerNames.some((name) => text.toUpperCase() === name.toUpperCase())) return false;
+
+		return true;
+	}
+
+	static invalidNameSubmitted() {
+		SpeedometerSelectPopup.invalidNameLabel.visible = true;
+	}
+
+	static onAddButtonPressed() {
+		this.textEntry.Submit();
+	}
+
 	static init() {
+		SpeedometerSelectPopup.invalidNameLabel.visible = false;
+		SpeedometerSelectPopup.speedometerNames = $.GetContextPanel()
+			.GetAttributeString('speedometerNames', '')
+			.split(',');
 		for (const typeNum of Object.values(SpeedometerTypes)) {
 			const speedometer = $.CreatePanel('Panel', SpeedometerSelectPopup.container, '');
 			speedometer.LoadLayoutSnippet('speedometer-radiobutton');
