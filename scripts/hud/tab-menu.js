26388004c1445d9bc6554b8a06b432b05f418b08
PeenScreeker <bumperjumper569@gmail.com>
2024-06-11 22:12:41 -0400

    feat: add button to toggle zone menu

--- scripts/hud/tab-menu.js
+++ scripts/hud/tab-menu.js
@@ -11,6 +11,10 @@ class HudTabMenu {
 		endOfRunContainer: $('#EndOfRunContainer'),
 		/** @type {Panel} @static */
 		zoningContainer: $('#ZoningContainer'),
+		/** @type {Panel} @static */
+		zoningOpenButton: $('#ZoningOpen'),
+		/** @type {Panel} @static */
+		zoningCloseButton: $('#ZoningClose'),
 		/** @type {Image} @static */
 		gamemodeImage: $('#HudTabMenuGamemodeImage'),
 		/** @type {Panel} @static */
@@ -42,14 +46,20 @@ class HudTabMenu {
 		this.panels.tabMenu.AddClass('hud-tab-menu--offset');
 		this.panels.leaderboardsContainer.AddClass('hud-tab-menu__leaderboards--hidden');
 		this.panels.endOfRunContainer.AddClass('hud-tab-menu__endofrun--hidden');
+
 		this.panels.zoningContainer.RemoveClass('hud-tab-menu__zoning--hidden');
+		this.panels.zoningOpenButton.AddClass('hud-tab-menu__zoning--hidden');
+		this.panels.zoningCloseButton.RemoveClass('hud-tab-menu__zoning--hidden');
 	}
 
 	static hideZoneMenu() {
 		this.panels.tabMenu.RemoveClass('hud-tab-menu--offset');
 		this.panels.leaderboardsContainer.RemoveClass('hud-tab-menu__leaderboards--hidden');
 		this.panels.endOfRunContainer.AddClass('hud-tab-menu__endofrun--hidden');
+
 		this.panels.zoningContainer.AddClass('hud-tab-menu__zoning--hidden');
+		this.panels.zoningOpenButton.RemoveClass('hud-tab-menu__zoning--hidden');
+		this.panels.zoningCloseButton.AddClass('hud-tab-menu__zoning--hidden');
 	}
 
 	static setMapData(isOfficial) {
