8c9fbabcdc41e6e6a6c53116d330649552ca4ae0
wertiop <wertiop121@gmail.com>
2024-07-27 11:27:57 +0100

    Fix style missing from panels inside a ConVarEnabler

--- scripts/pages/settings/settings.js
+++ scripts/pages/settings/settings.js
@@ -411,10 +411,6 @@ class MainMenuSettings {
 					if (this.isSettingsPanel(child) || child.HasClass('settings-group__combo')) {
 						this.styleItem(child, n);
 						n++;
-					}
-					// Otherwise if it's a ConVarEnabler search all its children
-					else if (child.paneltype === 'ConVarEnabler') {
-						for (const grandchild of child.Children()) search(grandchild);
 					} else {
 						search(child);
 					}
