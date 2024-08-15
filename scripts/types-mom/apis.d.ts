8f40b75e0ae8ec85e0973534ad7caca0be3b87ee
tsa96 <tom@trumpetdust.org>
2024-08-15 23:22:33 +0100

    feat: add LOADS of momentum types

---
+++ scripts/types-mom/apis.d.ts
@@ -0,0 +1,471 @@
+type Gamemode = import('common/web').Gamemode;
+
+declare namespace MomentumAPI {
+	/** Gets the local players level */
+	function GetPlayerXp(): int32;
+
+	/** Gets the local players XP */
+	function GetPlayerLevel(): int32;
+
+	/** Gets the total XP needed to reach a given level." */
+	function GetCosmeticXpForLevel(level: int32): int32;
+
+	/** Gets the amount of money the local player has (Units)." */
+	function GetPlayerMoney(): int32;
+	/** Gets Momentum's current version information." */
+
+	function GetVersionInfo(): { version: string };
+}
+
+declare namespace MomentumMovementAPI {
+	const enum PlayerMoveStatus {
+		AIR = 0,
+		WALK = 1,
+		WATER = 2,
+		WATERJUMP = 3
+	}
+
+	interface LastMove {
+		wishdir: vec3;
+		moveStatus: PlayerMoveStatus;
+		wishspeed: float;
+		acceleration: float;
+		maxspeed: float;
+		friction: float;
+		hasteTime: int32;
+		damageBoostTime: int32;
+		slickTime: int32;
+		defragTimer: int32;
+		defragTimerFlags: int32;
+	}
+
+	interface LastJump {
+		jumpCount: int32;
+		takeoffSpeed: float;
+		jumpSpeedDelta: float;
+		takeoffTime: float;
+		timeDelta: float;
+		strafeSync: float;
+		strafeCount: int32;
+		speedGain: float;
+		enviroAccel: float;
+		yawRatio: float;
+		heightDelta: float;
+		distance: float;
+		efficiency: float;
+	}
+
+	interface LastTick {
+		strafeRight: int32;
+		speedGain: float;
+		idealGain: float;
+		yawRatio: float;
+	}
+
+	const enum MoveType {
+		NONE = 0,
+		ISOMETRIC = 1,
+		WALK = 2,
+		STEP = 3,
+		FLY = 4,
+		FLYGRAVITY = 5,
+		VPHYSICS = 6,
+		PUSH = 7,
+		NOCLIP = 8,
+		LADDER = 9,
+		OBSERVER = 10,
+		CUSTOM = 11
+	}
+
+	/** Gets the current value of sv_stopspeed. */
+	function GetStopspeed(): float;
+
+	/** Gets the physics tick interval in seconds */
+	function GetTickInterval(): float;
+
+	/* Gets the current game time in seconds */
+	function GetCurrentTime(): float;
+
+	/** Gets the player's movetype (eg. normal, on a ladder, noclip, etc) */
+	function GetMoveType(): MoveType;
+
+	/** Gets an object containing the last move data */
+	function GetLastMoveData(): LastMove;
+
+	/* Gets an object containing stats from the last jump */
+	function GetLastJumpStats(): LastJump;
+
+	/** Gets an object containing gain stats from the last tick */
+	function GetLastTickStats(): LastTick;
+}
+
+declare namespace MomentumPlayerAPI {
+	/** Gets the player or spec target's current velocity */
+	function GetVelocity(): vec3;
+
+	/** Gets the player or spec target's current view angles */
+	function GetAngles(): vec3;
+
+	/** Gets the player or spec target's percentage of sync'd strafe ticks according to calculation type */
+	function GetStrafeSync(type: 0 | 1): float;
+
+	/** Gets the player's practice mode state */
+	function IsInPracticeMode(): boolean;
+
+	/** Gets the player's current FOV. */
+	function GetFOV(): float;
+
+	/** Gets whether the player is ducking. */
+	function IsDucking(): boolean;
+}
+
+declare namespace MomentumWeaponAPI {
+	/**
+	 * Gets the weapon slot for a specific weapon ID.
+	 * @returns -1 if not found.
+	 */
+	function GetWeaponSlot(weaponID: import('common/weapon').WeaponID): int32;
+}
+
+declare namespace MomentumReplayAPI {
+	/** Gets the current tick of the replay */
+	function GetCurrentTick(): int32;
+
+	/** Gets the total ticks of the replay */
+	function GetTotalTicks(): int32;
+
+	/** Gets the current time of the replay */
+	function GetCurrentTime(): float;
+
+	/** Gets the total time of the replay */
+	function GetTotalTime(): float;
+
+	/** Gets whether the replay is paused */
+	function IsPaused(): boolean;
+
+	/** Sets the progress of the replay */
+	function SetProgress(progress: float): void;
+}
+
+declare namespace ChatAPI {
+	function ChangeMuteState(steamID: number, mute: boolean): void;
+
+	function BIsUserMuted(steamID: number): boolean;
+}
+
+declare namespace SteamLobbyAPI {
+	/**
+	 * Request a data update. If new data is available, runs callbacks
+	 * @todo Filters haven't been implemented yet. For now, pass an empty object.
+	 */
+	function RefreshList(filters: Record<string, never>): boolean;
+
+	/** Create a new lobby with the given visibility */
+	function Create(type: 0 | 1 | 2): void;
+
+	/** Joins a lobby of the given SteamID */
+	function Join(steamID: steamID): void;
+
+	/** Leaves the current lobby */
+	function Leave(): void;
+
+	/** Change the lobby visibility */
+	function ChangeVisibility(type: 0 | 1 | 2): void;
+
+	/** Set the max players (up to 255) */
+	function SetMaxPlayers(maxPlayers: number): void;
+
+	/** Show the Steam Overlay invite dialog if we're in a lobby */
+	function ShowInviteDialog(): void;
+}
+
+declare namespace GameModeAPI {
+	type Gamemode = import('common/web').Gamemode;
+
+	/** Gets a random tip for the given gamemode. On first call, will load tips from file. */
+	function GetRandomTipForGameMode(gamemode: Gamemode): string;
+
+	/* Gets the current game mode type. */
+	function GetCurrentGameMode(): Gamemode;
+
+	/** Gets the name of a given gamemode type. */
+	function GetGameModeName(gamemode: Gamemode): string;
+
+	const enum GameModeHUDCapability {
+		SYNC = 0,
+		SYNC_BAR = 1,
+		KEYPRESS_STRAFES = 2,
+		KEYPRESS_JUMPS = 3,
+		KEYPRESS_ATTACK = 4,
+		KEYPRESS_WALK = 5,
+		KEYPRESS_SPRINT = 6
+	}
+
+	/** Gets whether the current gamemode has the specified HUD capability. */
+	function CurrentGameModeHasHUDCapability(capability: GameModeHUDCapability): boolean;
+}
+
+declare namespace MomentumTimerAPI {
+	/**
+	 * OLD API! Remove me!
+	 * @deprecated
+	 */
+	function GetTimerState(): any;
+	/**
+	 * OLD API! Remove me!
+	 * @deprecated
+	 */
+	function GetCurrentRunTime(): any;
+
+	type Zones = import('common/web').MapZones;
+
+	/** Gets the observed timer status */
+	function GetObservedTimerStatus(): import('common/timer').TimerStatus;
+
+	/** Gets the observed timer run splits */
+	function GetObservedTimerRunSplits(): import('common/timer').RunSplits;
+
+	/** Gets the ZoneDefs for the active zones, if any */
+	function GetActiveZoneDefs(): Zones;
+
+	/** Replace the active zones with the specified ZoneDefs */
+	function SetActiveZoneDefs(zoneDefs: Zones): void;
+
+	/** Save the specified ZoneDefs to newzones file */
+	function SaveZoneDefs(zoneDefs: Zones): void;
+}
+
+declare namespace MapCacheAPI {
+	type Map = import('common/web').MMap;
+	/** Get the current map's name */
+	function GetMapName(): string;
+
+	/** Get the metadata for the current map */
+	function GetCurrentMapData(): Map;
+
+	/** Gets all the maps from the map cache */
+	function GetMaps(): Map[];
+
+	/** Gets the map data for the given mapID */
+	function GetMapDataByID(mapID: int32): Map;
+
+	/** Returns true if the given mapID is queued from download */
+	function MapQueuedForDownload(mapID: int32): boolean;
+}
+
+declare namespace SpectatorAPI {
+	function GetSpecList(): steamID[];
+}
+
+/** Probably changing soon, doing weak types. */
+declare namespace RunComparisonsAPI {
+	function GetLoadedComparison(): any;
+	function IsComparisonLoaded(): any;
+	function GetLoadedComparisonSpeed(...args: any[]): any;
+	function GetLoadedComparisonOverallDiff(arg: any): any;
+}
+
+declare namespace ZonesAPI {
+	function GetZoneCount(...args: any[]): any; // TODO: Old API (I think)
+	function GetZoneSpeed(...args: any[]): any;
+	function GetZoneSpeed(...args: any[]): any;
+	function GetCurrentZone(...args: any[]): any;
+}
+
+declare namespace NewsAPI {
+	interface RSSFeedItem {
+		title: string;
+		description: string;
+		link: string;
+		date: string;
+		author: string;
+		image: string;
+	}
+
+	function GetRSSFeed(): void;
+}
+
+declare namespace SpeedometerSettingsAPI {
+	interface Settings {
+		custom_label: string;
+		enabled_axes: [boolean, boolean, boolean];
+		type: import('common/speedometer').SpeedometerType;
+		color_type: import('common/speedometer').SpeedometerColorType;
+		range_color_profile?: string;
+	}
+
+	/**
+	 * The naming of "Range" color as related is a bit confusing but too much work to refactor.
+	 * When you see a "RangeColor" it's better thought of as "Color Range".
+	 */
+	interface RangeColorProfile {
+		min: number;
+		max: number;
+		color: [number, number, number, number];
+	}
+
+	interface ColorProfile {
+		profile_name: string;
+		profile_ranges: RangeColorProfile[];
+	}
+
+	/** Gets speedometer settings for the current gamemode */
+	function GetCurrentGamemodeSettings(): Settings[];
+
+	/** Gets speedometer settings for a specific gamemode */
+	function GetSettings(gamemode: Gamemode): Settings[];
+
+	/** Resets speedometers for a specific gamemode to default */
+	function SaveSpeedometersFromJS(gamemode: Gamemode, settings: Settings[]): boolean;
+
+	/** Saves the range color profiles to file */
+	function ResetSpeedometersToDefault(gamemode: Gamemode): boolean;
+
+	/** Gets speedometer color profiles */
+	function GetColorProfiles(): ColorProfile[];
+
+	/** Gets a specified speedometer color profile */
+	function GetColorProfile(name: string): ColorProfile;
+
+	/** Saves the range color profiles to file */
+	function SaveColorProfilesFromJS(profiles: ColorProfile[]): boolean;
+
+	/** Resets color profiles to the default */
+	function ResetColorProfilesToDefault(): boolean;
+}
+
+declare namespace DefragAPI {
+	interface AccelConfig {
+		enable: boolean;
+		mirrorEnable: boolean;
+		mirrorBorder: float;
+		scaleEnable: boolean;
+		minSpeed: float;
+		height: float;
+		offset: float;
+		dzColor: rgbaColor;
+		fastColor: rgbaColor;
+		slowColor: rgbaColor;
+		turnColor: rgbaColor;
+	}
+
+	interface PrimeConfig {
+		enable: boolean;
+		truenessMode: int32;
+		inactiveEnable: boolean;
+		oneLineEnable: boolean;
+		minSpeed: float;
+		height: float;
+		offset: float;
+		gainColor: rgbaColor;
+		lossColor: rgbaColor;
+		altColor: rgbaColor;
+		highlightEnable: boolean;
+		highlightBorder: float;
+		highlightColor: rgbaColor;
+		scaleHeightEnable: boolean;
+		scaleColorEnable: boolean;
+		arrowEnable: boolean;
+		arrowSize: float;
+		arrowColor: rgbaColor;
+	}
+
+	interface SnapConfig {
+		enable: boolean;
+		enableHeightGain: boolean;
+		highlightMode: int32;
+		colorMode: int32;
+		minSpeed: float;
+		height: float;
+		offset: float;
+		color: rgbaColor;
+		altColor: rgbaColor;
+		highlightColor: rgbaColor;
+		altHighlightColor: rgbaColor;
+		fastColor: rgbaColor;
+		slowColor: rgbaColor;
+	}
+
+	interface WIndicatorConfig {
+		enable: boolean;
+		height: float;
+		offset: float;
+		size: float;
+		color: rgbaColor;
+		border: float;
+	}
+
+	interface CompassConfig {
+		compassMode: int32;
+		compassArrowSize: float;
+		compassTickSize: float;
+		compassOffset: float;
+		pitchEnable: boolean;
+		pitchTarget: string;
+		pitchWidth: float;
+		pitchOffset: float;
+		statMode: int32;
+		color: rgbaColor;
+		highlightColor: rgbaColor;
+	}
+
+	interface GroundboostConfig {
+		enable: boolean;
+		overshootEnable: boolean;
+		textMode: int32;
+		textColorMode: int32;
+		crashHlEnable: boolean;
+	}
+
+	const enum DefragPhysics {
+		VQ3 = 0,
+		CPM = 1,
+		VTG = 2
+	}
+
+	function GetDFPhysicsMode(): DefragPhysics;
+
+	function GetHUDProjection(): import('hud/cgaz').ProjectionMode;
+
+	function GetHUDFOV(): float;
+
+	function GetHUDAccelCFG(): AccelConfig;
+
+	function GetHUDPrimeCFG(): PrimeConfig;
+
+	function GetHUDSnapCFG(): SnapConfig;
+
+	function GetHUDWIndicatorCFG(): WIndicatorConfig;
+
+	function GetHUDCompassCFG(): CompassConfig;
+
+	function GetHUDGroundboostCFG(): GroundboostConfig;
+}
+
+/**
+ * API for reading and writing HUD config files.
+ *
+ * C++ side is agnostic to what you pass it, and we're not decided on the format of HUD layouts,
+ * so types here are deliberately very weak; types in the HUD customizer files are much stronger.
+ *
+ * Note that cfg/hud_default.kv3 is stored in a the licensee-only game repo, just let someone
+ * (probably Tom) know if you need to update it.
+ */
+declare namespace HudCustomizerAPI {
+	/**
+	 * Saves the given object to cfg/hud.kv3.
+	 */
+	function SaveLayoutFromJS(data: Record<string, any>): void;
+
+	/**
+	 * Tries to get the contents of cfg/hud.kv3 as a JS object.
+	 *
+	 * If cfg/hud.kv3 doesn't exist, loads cfg/hud_default.kv3.
+	 */
+	function GetLayout(): Record<string, any>;
+
+	/**
+	 * Gets the contents of cfg/hud_default.kv3 as a JS object.
+	 */
+	function GetDefaultLayout(): Record<string, any>;
+}
---
+++ scripts/types-mom/events.d.ts
@@ -0,0 +1,369 @@
+/**
+ * This file is for type definitions of events declared in and fired from C++.
+ * We need this file to be an ambient declaration file, so imports are banned. Unfortunately means we have to do
+ * endless ugly import('...') for instances of actual enums inside of common/ (const enums are a hassle since can't be
+ * iterated over).
+ */
+
+interface GlobalEventNameMap {
+	/** Tells JavaScript to show a confirm/deny popup that fires MapSelector_ConfirmOverwrite( mapID ) upon confirm option select. */
+	MapSelector_ShowConfirmOverwrite: (mapID: number) => void;
+
+	/** Confirms overwrite of the given map, allowing download. */
+	MapSelector_ConfirmOverwrite: (mapID: number) => void;
+
+	/** Removes the given mapID: number from the download queue. */
+	MapSelector_RemoveMapFromDownloadQueue: (mapID: number) => void;
+
+	/** Confirms the cancellation of the download for the given mapID: number. */
+	MapSelector_ConfirmCancelDownload: (mapID: number) => void;
+
+	/** Tries playing the given mapID: number. If it is not found locally, it is added to library and downloaded. */
+	MapSelector_TryPlayMap: (mapID: number) => void;
+
+	/** Like MapSelector_TryPlayMap, but takes the gamemode to override to before passing it along. */
+	MapSelector_TryPlayMap_GameModeOverride: (mapID: number, gamemode: import('common/web').Gamemode) => void;
+
+	/** Toggles adding/removing a map to/from favorites */
+	MapSelector_ToggleMapFavorite: (mapID: number, isAdding: boolean) => void;
+
+	/** Deletes a downloaded map */
+	MapSelector_DeleteMap: (mapID: number) => void;
+
+	/** Fired when the maps list has applied filters, with the number of maps that passed the filters passed along. */
+	MapSelector_MapsFiltered: (count: number) => void;
+
+	/** "Fired after TryPlayMap returns, if wasSuccessful the map was started, otherwise the map may be downloading or failed to start. */
+	MapSelector_TryPlayMap_Outcome: (wasSuccessful: boolean) => void;
+
+	/** Fired when the selected map has its data update */
+	MapSelector_SelectedDataUpdate: () => void;
+
+	PanoramaComponent_SteamLobby_OnListUpdated: (lobbyList: import('common/online').GroupedLobbyLists) => void;
+
+	PanoramaComponent_SteamLobby_OnDataUpdated: (lobbyData: import('common/online').LobbyList) => void;
+
+	PanoramaComponent_SteamLobby_OnMemberDataUpdated: (memberData: import('common/online').MemberData) => void;
+
+	PanoramaComponent_SteamLobby_OnMemberStateChanged: (
+		steamID: steamID,
+		change: import('common/online').LobbyMemberStateChange
+	) => void;
+
+	/** Fired when client state changes */
+	PanoramaComponent_SteamLobby_OnLobbyStateChanged: (change: import('common/online').LobbyMemberStateChange) => void;
+
+	PanoramaComponent_Chat_OnPlayerMuted: (steamID: steamID) => void;
+
+	PanoramaComponent_Chat_OnPlayerUnmuted: (steamID: steamID) => void;
+
+	PanoramaComponent_News_OnRSSFeedReceived: (feed: { items: NewsAPI.RSSFeedItem[] }) => void;
+
+	OnNewChatEntry: (panel: GenericPanel & { message: string; author_name: string; author_xuid: string }) => void;
+
+	/** Fired when the JS panel should update what it's showing */
+	HudCompare_Update: () => void;
+
+	// TODO: Old, remove after rio stuff is in
+	OnMomentumTimerStateChange: (arg1: any, arg2: any) => any;
+
+	OnCookUpdate: (time: float, percentage: float) => void; // Conc thing!
+
+	OnConcEntityPanelThink: () => void;
+
+	OnAimOverGhostChange: (panel: MomHudGhostEntityPanel, aimOver: boolean) => void;
+
+	// Some map cache stuff is probably wrong, not doing fully since probably changing soon
+	MapCache_MapLoad: (mapName: string) => void;
+
+	MapCache_CacheUpdate: (...args: unknown[]) => void; // TODO: Not typing since likely to change soon (sources are weird)
+
+	MapCache_MapUpdate: (...args: unknown[]) => void;
+
+	MapCache_SearchComplete: (success: boolean) => void;
+
+	MapDownload_Queued: (mapID: uint32, added: boolean) => void;
+
+	MapDownload_Start: (mapID: uint32, mapName: string) => void;
+
+	MapDownload_Size: (mapID: uint32, size: uint64_str) => void;
+
+	MapDownload_Progress: (mapID: uint32, sizeOfChunk: uint64_str, offsetOfChunk: uint64_str) => void;
+
+	MapDownload_End: (mapID: uint32, error: boolean) => void;
+
+	Game_MetaModeChanged: (gamemode: import('common/web').Gamemode) => void;
+
+	OnMomentumWeaponStateChange: (
+		state: import('common/weapon').WeaponStateChangeMode,
+		weaponID: import('common/weapon').WeaponID
+	) => void;
+
+	OnAllMomentumWeaponsDropped: () => void;
+
+	OnDefragHUDProjectionChange: () => void;
+
+	OnDefragHUDFOVChange: () => void;
+
+	OnDefragHUDAccelChange: () => void;
+
+	OnDefragHUDSnapChange: () => void;
+
+	OnDefragHUDPrimeChange: () => void;
+
+	OnDefragHUDWIndicatorChange: () => void;
+
+	OnDefragHUDCompassChange: () => void;
+
+	OnDefragHUDGroundboostChange: () => void;
+
+	DFJumpDataUpdate: (releaseDelay: float, pressDelay: float, totalDelay: float) => void;
+
+	DFJumpMaxDelayChanged: (newDelay: float) => void;
+
+	OnJumpStatsCFGChange: () => void;
+
+	OnJumpStarted: () => void;
+
+	/** Called when the player changes synchronizer display mode setting (mom_hud_synchro_mode). */
+	OnSynchroModeChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer color mode setting (mom_hud_synchro_color_enable). */
+	OnSynchroColorModeChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer dynamic direction setting (mom_hud_synchro_dynamic_enable). */
+	OnSynchroDynamicModeChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer direction setting  (mom_hud_synchro_flip_enable). */
+	OnSynchroDirectionChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer buffer length setting (mom_hud_synchro_buffer_size). */
+	OnSynchroBufferChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer minimum speed setting (mom_hud_synchro_min_speed). */
+	OnSynchroMinSpeedChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer text mode setting (mom_hud_synchro_stat_mode)." */
+	OnSynchroStatModeChanged: (cvarValue: int32) => void;
+
+	/** Called when the player changes synchronizer stats color mode setting (mom_hud_synchro_color_enable). */
+	OnSynchroStatColorModeChanged: (cvarValue: int32) => void;
+
+	/** Fires when quit_prompt is called. */
+	OnMomentumQuitPrompt: () => void;
+
+	/** Fired when HUD chat is opened. */
+	HudChat_Show: () => void;
+
+	/** Fired when HUD chat is closed. */
+	HudChat_Hide: () => void;
+
+	/** Called on speed update, respecting mom_hud_speedometer_fps */
+	OnSpeedometerUpdate: (deltaTime: float) => void;
+
+	/** Called on explosive hit speed update */
+	OnExplosiveHitSpeedUpdate: (velocity: vec3) => void;
+
+	/** Called on jump speed update */
+	OnJumpSpeedUpdate: (speed: float) => void;
+
+	/** Called on ramp board speed update */
+	OnRampBoardSpeedUpdate: (velocity: vec3) => void;
+
+	/** Called on ramp leave speed update */
+	OnRampLeaveSpeedUpdate: (velocity: vec3) => void;
+
+	/** Called when speedometer settings have been loaded */
+	OnSpeedometerSettingsLoaded: (success: boolean) => void;
+
+	/** Called when range color profiles have been loaded */
+	OnRangeColorProfilesLoaded: (success: boolean) => void;
+
+	/** Called when speedometer settings have been saved */
+	OnSpeedometerSettingsSaved: (success: boolean) => void;
+
+	/** Called when range color profiles have been saved */
+	OnRangeColorProfilesSaved: (success: boolean) => void;
+
+	/**
+	 * Fired from C++ when we want to create a standard Toast panel.
+	 * @internal Don't use these, import ToastManager and use that directly.
+	 */
+	Toast_Show: (
+		id: string,
+		title: string,
+		message: string,
+		location: import('util/toast-manager').ToastLocation,
+		duration: number,
+		icon: string,
+		style: string
+	) => void;
+
+	/**
+	 * Fired from C++ when we want to create a custom Toast panel.
+	 * @internal Don't use these, import ToastManager and use that directly.
+	 */
+	Toast_ShowCustom: (
+		id: string,
+		layoutFile: string,
+		location: import('util/toast-manager').ToastLocation,
+		duration: number,
+		parameters: Record<string, any>
+	) => void;
+
+	/**
+	 * Fired from C++ to delete toasts
+	 * @internal Don't use these, import ToastManager and use that directly.
+	 */
+	Toast_Delete: (id: string) => void;
+
+	/**
+	 * Fired from C++ to clear toastsa
+	 * @internal Don't use these, import ToastManager and use that directly.
+	 */
+	Toast_Clear: (location?: import('util/toast-manager').ToastLocation) => void;
+
+	MomentumSpectatorTargetChanged: (type: import('common/timer').RunEntityType) => void;
+
+	MomentumSpectatorModeChanged: (mode: SpectateMode) => void;
+
+	MomentumSpecListMaxNamesUpdate: (maxNames: uint32) => void;
+
+	MomentumSpectateStart: () => void;
+
+	MomentumSpectateStop: () => void;
+
+	/** Called whgen lobby-wide spectator state changes */
+	MomentumSpectatorUpdate: () => void;
+
+	/**
+	 * TODO: changing with new zones/timer
+	 * @deprecated
+	 */
+	OnMomentumZoneChange: (
+		entering: boolean,
+		isLinear: boolean,
+		currentZone: int32,
+		currentTrack: int32,
+		timerState: import('common/timer').TimerState_OLD
+	) => void;
+
+	OnMomentumReplayStarted: () => void;
+
+	OnMomentumReplayStopped: () => void;
+
+	OnChargeUpdate: (speedInUps: float, percentageCharge: float) => void;
+
+	OnChargeToggled: (enabled: boolean) => void;
+
+	OnStickyPanelStateChanged: (
+		panel: Panel,
+		state: import('hud/sticky-count').StickyState,
+		prevState: import('hud/sticky-count').StickyState
+	) => void;
+
+	SteamLobby_Enter: (lobbyID: steamID) => void;
+
+	SteamLobby_Exit: () => void;
+
+	SteamLobby_MemberJoinLeave: (lobbyID: steamID, memberID: steamID) => void;
+
+	OnMomentumPlayerPracticeModeStateChange: (enabled: boolean) => void;
+
+	OnSaveStateUpdate: (count: int32, current: int32, usingMenu: boolean) => void;
+
+	/** Fired when the HUD Leaderboards panel is opened */
+	HudTabMenu_Opened: () => void;
+
+	/** Fired when the HUD Leaderboards panel is closed */
+	HudTabMenu_Closed: () => void;
+
+	/** Fired when the HUD leaderboards panel gains mouse input */
+	HudTabMenu_OnMouseActive: () => void;
+
+	/** Fired when the HUD leaderboards panel should show the end of run panel */
+	EndOfRun_Show: (reason: import('common/timer').EndOfRunShowReason) => void;
+
+	/** Fired when the end of run panel should be hidden */
+	EndOfRun_Hide: () => void;
+
+	/** Fired when the end of run panel should set the two runs to compare. baseRun is compared to compareRun */
+	EndOfRun_CompareRuns: (
+		baseRun: import('common/timer').CPPRun_OLD,
+		compareRun: import('common/timer').CPPRun_OLD
+	) => void;
+
+	/** Fired when the replay recording finishes and passes whether writing the file was successful */
+	EndOfRun_Result_RunSave: (saved: boolean) => void;
+
+	/** Fired when the replay recording finishes and passes whether writing the file was successful */
+	EndOfRun_Result_RunUpload: (uploaded: boolean, cosXP: number, rankXP: number, lvlGain: number) => void;
+
+	ZoneMenu_Show: () => void;
+
+	ZoneMenu_Hide: () => void;
+
+	OnPointPicked: (point: vec3) => void;
+
+	OnPickCanceled: () => void;
+
+	/** Fired when the the primary timer of the UI entity transitions to a different state */
+	OnObservedTimerStateChange: (trackID: import('common/timer').TrackID) => void;
+
+	/** Fired when the the primary timer of the UI entity progresses to a new checkpoint */
+	OnObservedTimerCheckpointProgressed: (trackId: import('common/timer').TrackID) => void;
+
+	/** Fired when the the primary timer of the UI entity effectively starts a segment. */
+	OnObservedTimerSegmentEffectiveStart: (trackId: import('common/timer').TrackID) => void;
+
+	/** Fired when the UI entity changes or when primary timer of the UI entity changes */
+	OnObservedTimerReplaced: () => void;
+
+	/** Fired when the selected time list type is changed, passing the old times list type and new type that was selected */
+	Leaderboards_TimeListTypeChange: (
+		oldType: import('common/leaderboard').LeaderboardType,
+		newType: import('common/leaderboard').LeaderboardType
+	) => void;
+
+	/** Fired when the leaderboards is applying filters to the list, and passes the number of maps to display */
+	Leaderboards_TimesFiltered: (numMaps: int32) => void;
+
+	/** Fired when the leaderboards has its map data set but before times are loaded */
+	Leaderboards_MapDataSet: (isOfficial: boolean) => void;
+
+	/** Fired when the map leaderboard data has loaded */
+	Leaderboards_MapLeaderboardsLoaded: (map: import('common/web').MMap) => void;
+
+	LeaderboardEntry_TimeDataUpdated: () => void;
+
+	LeaderboardEntry_PlayReplay: (itemIndex: int32) => void;
+
+	LeaderboardEntry_DeleteReplay: (itemIndex: int32) => void;
+
+	/**
+	 * This event is fired when the act of disconnecting from the map is safeguarded.
+	 * Respond to this via dispatching a Safeguard_Response event if the user acccepts.
+	 */
+	Safeguard_Disconnect: () => void;
+
+	/**
+	 * This event is fired when the act of quitting the game is safeguarded.
+	 * Respond to this via dispatching a Safeguard_Response event if the user acccepts.
+	 */
+	Safeguard_Quit: () => void;
+
+	/**
+	 * This event is fired when the act of changing the map is safeguarded.
+	 * Respond to this via dispatching a Safeguard_Response event if the user acccepts.
+	 */
+	Safeguard_ChangeMap: (mapName: string) => void;
+
+	/**
+	 * This event is supposed to be fired back to C++ code to be able to handle the outcome of the Safeguard popup response.
+	 */
+	Safeguard_Response: (type: import('common/safeguards').RunSafeguardType) => void;
+}
+
+interface PanelEventNameMap {
+	MapEntry_MapDataUpdate: () => void;
+}
