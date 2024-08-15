ddef224925286401d8669bf2d2b30965645c9fda
tsa96 <tom@trumpetdust.org>
2024-08-15 23:52:56 +0100

    refactor: remove levels-explainer popup JS, replace with inline script
    Don't like having a dedicated script for something simple as this, better in <script> tag.

--- scripts/modals/context-menus/levels-explainer.ts
+++
@@ -1,8 +0,0 @@
-class LevelsExplainer {
-	static fixLevelIndicators() {
-		// When these panels are first created, for some godforsaken reason they don't have the correct class until you
-		// mouse over them... I have absolutely no idea why, this is a quick fix.
-		const className = 'levels-explainer__level-indicator';
-		for (const p of $.GetContextPanel().FindChildrenWithClassTraverse(className)) p.TriggerClass(className);
-	}
-}
