1df6acfd6346ed3185c492e4908eb71b9a923b60
tsa96 <tom@trumpetdust.org>
2025-04-16 18:22:20 +0100

    refactor: update HudStatus

--- scripts/hud/status.ts
+++ scripts/hud/status.ts
@@ -5,48 +5,75 @@ import { TrackType } from 'common/web';
 
 @PanelHandler()
 class HudStatusHandler {
-	label = $<Label>('#HudStatusLabel');
+	readonly panels = {
+		cp: $.GetContextPanel<MomHudStatus>(),
+		label: $<Label>('#HudStatusLabel')!
+	};
 
 	constructor() {
-		$.RegisterForUnhandledEvent('OnMomentumPlayerPracticeModeStateChange', (enabled) =>
-			this.onPracticeModeChange(enabled)
-		);
-		$.RegisterForUnhandledEvent('OnSaveStateUpdate', (count, current, usingMenu) =>
-			this.onSaveStateChange(count, current, usingMenu)
-		);
-		$.RegisterForUnhandledEvent('OnObservedTimerStateChange', () => this.update());
-		$.RegisterForUnhandledEvent('OnObservedTimerCheckpointProgressed', () => this.update());
-		$.RegisterForUnhandledEvent('OnObservedTimerReplaced', () => this.update());
-
 		$.GetContextPanel<MomHudStatus>().hiddenHUDBits = HideHud.TABMENU;
 
-		this.label.text = $.Localize('#HudStatus_Spawn');
+		$.RegisterForUnhandledEvent('OnObservedTimerStateChange', () => {
+			this.update();
+		});
+
+		$.RegisterForUnhandledEvent('OnObservedTimerCheckpointProgressed', () => {
+			this.update();
+		});
+
+		$.RegisterForUnhandledEvent('OnObservedTimerReplaced', () => {
+			this.timerInteractedWith = true;
+			// Don't show savestate stuff if we start watching replay/specing
+			// Though note you don't get savestate stuff back on exiting replay/specing
+			// until you do savestate stuff again - don't have a good solution to that.
+			this.saveStatesActive = false;
+			this.update();
+		});
+
+		$.RegisterForUnhandledEvent('OnMomentumPlayerPracticeModeStateChange', (enabled) => {
+			this.inPracticeMode = enabled;
+			this.timerInteractedWith = true;
+			this.update();
+		});
+
+		// Called when savestates added, removed, all removed, teleported to, menu closed
+		$.RegisterForUnhandledEvent('OnSaveStateUpdate', (count, current, usingMenu) => {
+			// usingMenu has very specific behaviour; defaults to false,
+			// set true when you teleport to a saveloc (incl with console command), not on saveloc ceration
+			// set false when menu is closed, and on level shutdown
+			//
+			// So toggling whether we show savestate info in status based on this is okay - presumably intended.
+			// It become enabled until you actually teleport to a savestate,
+			// then stays enabled until menu closes or level changes.
+			this.saveStatesActive = usingMenu;
+			this.saveStateCount = count;
+			this.saveStateCurrent = current + 1; // need 1-indexing for display
+
+			this.timerInteractedWith = true;
+			this.update();
+		});
+
+		$.RegisterForUnhandledEvent('LevelInitPostEntity', () => {
+			this.timerInteractedWith = false;
+			this.saveStatesActive = false;
+			this.inPracticeMode = false;
+			this.update();
+		});
 	}
+
+	saveStatesActive = false;
+	saveStateCount = 0;
+	saveStateCurrent = 0;
 
 	inPracticeMode = false;
 
-	saveStateCount = 0;
-	saveStateCurrent = 0;
-	saveStateUsing = false;
-
-	onPracticeModeChange(enabled: boolean) {
-		this.inPracticeMode = enabled;
-		this.update();
-	}
-
-	onSaveStateChange(count: number, current: number, usingMenu: boolean) {
-		this.saveStateCount = count;
-		this.saveStateCurrent = current + 1; // need 1-indexing for display
-		this.saveStateUsing = usingMenu;
-
-		this.update();
-	}
+	// Whether player has doing anything timer related yet, e.g. primed, started, savelocced, used practice mode.
+	timerInteractedWith = false;
 
 	update() {
 		let text = this.getTimerText();
 
-		// TODO: maybe show these somewhere else instead of prepending tons of stuff
-		if (this.saveStateUsing) {
+		if (this.saveStatesActive) {
 			text = `${this.strs.saveState} ${this.saveStateCurrent}/${this.saveStateCount} | ${text}`;
 		}
 
@@ -54,7 +81,9 @@ class HudStatusHandler {
 			text = `${this.strs.practiceMode} | ${text}`;
 		}
 
-		this.label.text = text;
+		this.panels.cp.SetHasClass('hudstatus--hidden', !this.timerInteractedWith);
+
+		this.panels.label.text = text;
 	}
 
 	private getTimerText(): string {
@@ -62,15 +91,27 @@ class HudStatusHandler {
 			MomentumTimerAPI.GetObservedTimerStatus();
 
 		if (state === TimerState.DISABLED) {
-			return $.Localize('#HudStatus_TimerDisabled');
+			return this.strs.disabled;
 		}
 
+		// Timer has been interacted with if it's not disabled
+		this.timerInteractedWith = true;
+
 		if (state === TimerState.PRIMED) {
-			return $.Localize('#HudStatus_TimerPrimed');
+			let str = this.strs.primed + ' | ';
+			if (trackId.type === TrackType.MAIN) {
+				str += this.strs.mainTrack;
+			} else if (trackId.type === TrackType.STAGE) {
+				str += `${this.strs.stage} ${trackId.number}`;
+			} else {
+				str += `${this.strs.bonus} ${trackId.number}`;
+			}
+
+			return str;
 		}
 
 		if (state === TimerState.FINISHED) {
-			return $.Localize('#HudStatus_TimerFinished');
+			return this.strs.finished;
 		}
 
 		// state is TimerState.RUNNING
@@ -96,10 +137,14 @@ class HudStatusHandler {
 
 	// Cache strings to save endless $.Localize calls
 	readonly strs = {
-		cp: $.Localize('#HudStatus_Checkpoint'),
-		stage: $.Localize('#HudStatus_Stage'),
-		bonus: $.Localize('#HudStatus_Bonus'),
-		saveState: $.Localize('#HudStatus_SaveState'),
-		practiceMode: $.Localize('#HudStatus_PracticeMode')
+		disabled: $.Localize('#Timer_Disabled'),
+		primed: $.Localize('#Timer_Primed'),
+		finished: $.Localize('#Timer_Finished'),
+		mainTrack: $.Localize('#Timer_Main'),
+		cp: $.Localize('#Timer_Checkpoint'),
+		stage: $.Localize('#Timer_Stage'),
+		bonus: $.Localize('#Timer_Bonus'),
+		saveState: $.Localize('#Timer_SaveState'),
+		practiceMode: $.Localize('#Timer_PracticeMode')
 	};
 }
