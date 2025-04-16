afb930056e92e5558438120a1be16bedce1f2570
tsa96 <tom@trumpetdust.org>
2025-04-16 18:53:55 +0100

    refactor: update HudSpectate

--- scripts/hud/spectate.ts
+++ scripts/hud/spectate.ts
@@ -1,26 +1,46 @@
-import { PanelHandler } from 'util/module-helpers';
+import { OnPanelLoad, PanelHandler } from 'util/module-helpers';
 
 @PanelHandler()
-class HudSpectateHandler {
+class HudSpectateHandler implements OnPanelLoad {
 	readonly panels = {
-		indicatorSpectating: $('#IndicatorSpectating'),
-		indicatorWatchingReplay: $('#IndicatorWatchingReplay'),
-		//targetName: $('#TargetName'),
-		//prevPlayer: $('#PrevPlayer'),
-		//nextPlayer: $('#NextPlayer'),
-		replayControls: $('#ReplayControls'),
-		toggleReplayControls: $('#ToggleReplayControls')
+		cp: $.GetContextPanel(),
+		statusIcon: $<Image>('#StatusIcon')!,
+		prevPlayer: $<Button>('#PrevPlayer')!,
+		nextPlayer: $<Button>('#NextPlayer')!,
+		replayControls: $<MomHudReplayControls>('#ReplayControls')!,
+		toggleReplayControls: $<Button>('#ToggleReplayControls')!
 	};
 
-	constructor() {
-		// TEMP: Hide spectate panel since we don't distinguish between replays and spectate yet
-		this.panels.indicatorSpectating.visible = false;
+	onPanelLoad() {
+		// TODO: We don't have a way to distinguish between speccing and watching replay yet,
+		// needs rio code. Replay controls will be permanently visible until this.
+		this.setReplayMode(true);
 
 		// $.RegisterForUnhandledEvent('MomentumSpectatorTargetChanged', (type: RunEntityType) =>
 		// 	this.onSpectatorChanged(type)
 		// );
 	}
 
+	setReplayMode(enable: boolean) {
+		this.panels.cp.SetDialogVariable(
+			'status',
+			$.Localize(enable ? '#Spectate_Status_WatchingReplay' : '#Spectate_Status_Spectating')
+		);
+
+		this.panels.statusIcon.SetImage(`file://{images}/${enable ? 'movie-open-outline' : 'eye'}.svg`);
+
+		this.panels.prevPlayer.visible = !enable;
+		this.panels.nextPlayer.visible = !enable;
+		this.panels.toggleReplayControls.visible = enable;
+
+		this.panels.replayControls.hidden = !enable;
+	}
+
+	toggleReplayControls() {
+		this.panels.replayControls.hidden = !this.panels.replayControls.hidden;
+	}
+
+	// TODO: Old handler for spectator changed event
 	/*onSpectatorChanged(type: RunEntityType) {
 		if (type !== RunEntityType.PLAYER) {
 			const isReplay = type === RunEntityType.REPLAY;
