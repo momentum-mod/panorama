c0713a89e0f610e19c639bee36241d00c95def66
tsa96 <tom@trumpetdust.org>
2025-06-16 23:39:39 +0100

    tmp: allow getOrCreateGlobal to handle regular objects
    Just something I wrote whilst debugging toasts. Note that I've removing
    this whole function in the next commit (it's buggy and overcomplex) but
    we *might* add it back in the future so getting this version tracked in
    Git.

--- scripts/util/module-helpers.ts
+++ scripts/util/module-helpers.ts
@@ -41,12 +41,39 @@ export function exposeToPanelContext(obj: Record<string, any>): void {
  * Only one instance will be created for the lifetime of Panorama, allowing module scripts/modules
  * to access the same instance, without `useglobalcontext`.
  */
-export function getOrCreateGlobal<T>(name: string, ctor: Constructor<T>, ...args: unknown[]): T {
+export function getOrCreateGlobal<T>(name: string, ctor: Constructor<T>, ...args: unknown[]): T;
+
+/**
+ * Get or create a globally accessible object.
+ *
+ * Only one instance will be created for the lifetime of Panorama, allowing module scripts/modules
+ * to access the same instance, without `useglobalcontext`.
+ *
+ * If the object already exists, a reference the existing object is returned, otherwise the passed
+ * object reference is set on the global object, and returned.
+ */
+export function getOrCreateGlobal<T extends object>(name: string, obj: T): T;
+
+export function getOrCreateGlobal<Class, Obj extends object>(
+	name: string,
+	global: Constructor<Class> | Obj,
+	...args: unknown[]
+): Class | Obj {
 	const globalObject = UiToolkitAPI.GetGlobalObject();
-	if (!(name in globalObject)) {
-		globalObject[name] = new ctor(...args);
+
+	if (typeof global === 'function') {
+		if (!(name in globalObject)) {
+			globalObject[name] = new global(...args);
+		}
+
+		return globalObject[name] as Class;
+	} else {
+		if (!(name in globalObject)) {
+			globalObject[name] = global;
+		}
+
+		return globalObject[name] as Obj;
 	}
-	return globalObject[name] as T;
 }
 
 /**
