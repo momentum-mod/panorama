ed6a804bb622bafe2b29abf6bedf17c29546a4ba
Panzer <tylerbeff@gmail.com>
2025-07-21 15:19:35 -0600

    feat: Run style dropdown

--- scripts/types-mom/apis.d.ts
+++ scripts/types-mom/apis.d.ts
@@ -293,6 +293,9 @@ declare namespace GameModeAPI {
 	 * Returns #Timer_Checkpoint for most modes, #Timer_Jump for modes like RJ.
 	 */
 	function GetGameModeCheckpointToken(gamemode: Gamemode): string;
+
+	/** Gets all valid run styles for the provided gamemode. */
+	function GetValidRunStyles(gamemode: Gamemode): number[];
 }
 
 declare namespace MomentumTimerAPI {
