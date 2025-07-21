1f708449a9dcaa1a65853476b7a3c5f3bbd39dd9
Panzer <tylerbeff@gmail.com>
2025-07-21 15:19:35 -0600

    feat: Run style dropdown

---
+++ scripts/common/run-style.ts
@@ -0,0 +1,22 @@
+// This needs to be in sync with the RunStyle enum in C++.
+export enum RunStyle {
+	NORMAL = 0,
+	HALF_SIDEWAYS,
+	SIDEWAYS,
+	W_ONLY,
+	AD_ONLY,
+	S_ONLY,
+	BACKWARDS,
+	NO_TELEPORT
+}
+
+export const RunStyleNames: ReadonlyMap<RunStyle, string> = new Map([
+	[RunStyle.NORMAL, '#RunStyle_Normal'],
+	[RunStyle.HALF_SIDEWAYS, '#RunStyle_HalfSideways'],
+	[RunStyle.SIDEWAYS, '#RunStyle_Sideways'],
+	[RunStyle.W_ONLY, '#RunStyle_WOnly'],
+	[RunStyle.AD_ONLY, '#RunStyle_ADOnly'],
+	[RunStyle.S_ONLY, '#RunStyle_SOnly'],
+	[RunStyle.BACKWARDS, '#RunStyle_Backwards'],
+	[RunStyle.NO_TELEPORT, '#RunStyle_NoTeleport']
+]);
