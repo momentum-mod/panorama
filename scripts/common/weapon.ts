891adb92180533c9cd1b97fb78f7d11466eff5bc
tsa96 <tom@trumpetdust.org>
2025-08-04 06:29:06 +0100

    feat: add defrag weapons to common/weapon.ts

--- scripts/common/weapon.ts
+++ scripts/common/weapon.ts
@@ -15,7 +15,16 @@ export enum WeaponID {
 	KNIFE = 7,
 	ROCKETLAUNCHER = 8,
 	STICKYLAUNCHER = 9,
-	CUBEMAP = 10
+	DF_PLASMAGUN = 10,
+	DF_ROCKETLAUNCHER = 11,
+	DF_BFG = 12,
+	DF_GRENADELAUNCHER = 13,
+	DF_MACHINEGUN = 14,
+	DF_RAILGUN = 15,
+	DF_LIGHTNINGGUN = 16,
+	DF_SHOTGUN = 17,
+	DF_KNIFE = 18,
+	CUBEMAP = 19
 }
 
 export const WeaponNames: ReadonlyMap<WeaponID, string> = new Map([
@@ -28,5 +37,14 @@ export const WeaponNames: ReadonlyMap<WeaponID, string> = new Map([
 	[WeaponID.KNIFE, '#Weapon_Knife'],
 	[WeaponID.ROCKETLAUNCHER, '#Weapon_RocketLauncher'],
 	[WeaponID.STICKYLAUNCHER, '#Weapon_StickybombLauncher'],
+	[WeaponID.DF_PLASMAGUN, '#Weapon_PlasmaGun'],
+	[WeaponID.DF_ROCKETLAUNCHER, '#Weapon_RocketLauncher'],
+	[WeaponID.DF_BFG, '#Weapon_BFG'],
+	[WeaponID.DF_GRENADELAUNCHER, '#Weapon_GrenadeLauncher'],
+	[WeaponID.DF_MACHINEGUN, '#Weapon_MachineGun'],
+	[WeaponID.DF_RAILGUN, '#Weapon_Rail'],
+	[WeaponID.DF_LIGHTNINGGUN, '#Weapon_LightningGun'],
+	[WeaponID.DF_SHOTGUN, '#Weapon_Shotgun'],
+	[WeaponID.DF_KNIFE, '#Weapon_Knife'],
 	[WeaponID.CUBEMAP, '#Weapon_Cubemap']
 ]);
