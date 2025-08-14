23e00266525c28c1187ae7980aff70f012abca12
tsa96 <tom@trumpetdust.org>
2025-08-15 00:18:23 +0100

    feat: show player color preview panels in settings
    These are little panels on top right of model viewer, helps with seeing
    exactly what color the emissive is

--- scripts/pages/settings/page.ts
+++ scripts/pages/settings/page.ts
@@ -1,6 +1,7 @@
 import { PanelHandler } from 'util/module-helpers';
 import { isSettingsPanel } from 'common/settings';
 import { traverseChildren } from 'util/functions';
+import * as Colors from 'util/colors';
 
 @PanelHandler()
 export class SettingsPage {
@@ -30,6 +31,15 @@ export class SettingsPage {
 				break;
 			}
 			case 'OnlineSettings': {
+				$.RegisterConVarChangeListener('mom_playercolor_primary_hue', () => this.onlineSettingsUpdateModel());
+				$.RegisterConVarChangeListener('mom_playercolor_primary_saturation', () =>
+					this.onlineSettingsUpdateModel()
+				);
+				$.RegisterConVarChangeListener('mom_playercolor_primary_value', () => this.onlineSettingsUpdateModel());
+				$.RegisterConVarChangeListener('mom_playercolor_secondary_hue', () => this.onlineSettingsUpdateModel());
+				$.RegisterConVarChangeListener('mom_playercolor_secondary_saturation', () =>
+					this.onlineSettingsUpdateModel()
+				);
 				this.onlineSettingsUpdateModel();
 
 				break;
@@ -169,20 +179,47 @@ export class SettingsPage {
 		paintPanel.style.width = scale * width + 'px';
 	}
 
+	ghostPreview: ModelPanel;
+	ghostPrimaryColor: Panel;
+	ghostSecondaryColor: Panel;
+
 	onlineSettingsUpdateModel() {
-		const onlineSettingsPanel = $('#OnlineSettings');
-		const ghostPreview = onlineSettingsPanel.FindChildInLayoutFile<ModelPanel>('GhostModelPreview');
+		// Silly design of settings page JS means we need logic like this to cache panels
+		if (!this.ghostPreview) {
+			const onlineSettings = $('#OnlineSettings');
+			this.ghostPreview = onlineSettings.FindChildInLayoutFile<ModelPanel>('GhostModelPreview');
+			this.ghostPrimaryColor = onlineSettings.FindChildInLayoutFile('GhostPrimaryColor');
+			this.ghostSecondaryColor = onlineSettings.FindChildInLayoutFile('GhostSecondaryColor');
+		}
 
-		ghostPreview.SetCameraFOV(60);
-		ghostPreview.SetModelRotation(0, 180, 0); // Model faces away from us by default, rotate it towards us
-		ghostPreview.SetMouseXRotationScale(0, 1, 0); // By default mouse X will rotate the X axis, but we want it to spin Y axis
-		ghostPreview.SetMouseYRotationScale(0, 0, 0); // Disable mouse Y movement rotations
-		ghostPreview.LookAtModel();
-		ghostPreview.SetCameraOffset(-65, 0, 0);
+		this.ghostPreview.SetCameraFOV(50);
+		this.ghostPreview.SetModelRotation(0, 180, 0); // Model faces away from us by default, rotate it towards us
+		this.ghostPreview.SetMouseXRotationScale(0, 1, 0); // By default mouse X will rotate the X axis, but we want it to spin Y axis
+		this.ghostPreview.SetMouseYRotationScale(0, 0, 0); // Disable mouse Y movement rotations
+		this.ghostPreview.LookAtModel();
+		this.ghostPreview.SetCameraOffset(-65, 0, 0);
 
-		ghostPreview.SetLightAmbient(1, 1, 1);
-		ghostPreview.SetDirectionalLightColor(0, 2, 2, 2);
-		ghostPreview.SetDirectionalLightDirection(0, 1, 1, -1);
+		this.ghostPreview.SetLightAmbient(1, 1, 1);
+		this.ghostPreview.SetDirectionalLightColor(0, 2, 2, 2);
+		this.ghostPreview.SetDirectionalLightDirection(0, 1, 1, -1);
+
+		this.ghostPrimaryColor.style.backgroundColor = Colors.tupleToRgbaString(
+			Colors.hsvaToRgba([
+				GameInterfaceAPI.GetSettingFloat('mom_playercolor_primary_hue'),
+				GameInterfaceAPI.GetSettingFloat('mom_playercolor_primary_saturation'),
+				GameInterfaceAPI.GetSettingFloat('mom_playercolor_primary_value'),
+				255
+			])
+		);
+
+		this.ghostSecondaryColor.style.backgroundColor = Colors.tupleToRgbaString(
+			Colors.hsvaToRgba([
+				GameInterfaceAPI.GetSettingFloat('mom_playercolor_secondary_hue'),
+				GameInterfaceAPI.GetSettingFloat('mom_playercolor_secondary_saturation'),
+				1,
+				255
+			])
+		);
 	}
 
 	initTextureReplacementDropdown() {
