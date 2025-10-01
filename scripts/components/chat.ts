d668ffde573723ec07efcd15903757790e24af67
tsa96 <tom@trumpetdust.org>
2025-10-01 02:12:20 +0100

    fix: scroll chat to bottom on open / new messages
    Not doing the mute thing since it's sooooo many event handlers (have to
    be recreated whenever you change map as well)

--- scripts/components/chat.ts
+++ scripts/components/chat.ts
@@ -5,12 +5,26 @@ import { MemberData } from 'common/online';
 class ChatHandler {
 	membersTyping: string[] = [];
 	typingLabel = $<Label>('#ChatMemberTypingLabel');
+	history = $('#ChatHistory');
 
 	constructor() {
 		$.RegisterEventHandler('OnNewChatEntry', $.GetContextPanel(), (panel) => this.onNewChatEntry(panel));
 
 		$.RegisterForUnhandledEvent('PanoramaComponent_SteamLobby_OnMemberDataUpdated', (memberData: MemberData) =>
 			this.onSteamLobbyMemberDataUpdated(memberData)
+		);
+
+		// Hacks to scroll to bottom when opening chat / joining/leaving a lobby.
+		// Whether or not scrolling works seems to corresponding to IsReadyForDisplay(), but
+		// I can't get RegisterForReadyEvents/ReadyForDisplay to fire at all.
+		// This timing appears consistent in debug so *hopefully* fine for everyone,
+		// but obviously an event-based approach would be preferred in the future.
+		$.RegisterForUnhandledEvent('HudChat_Show', () => {
+			$.Schedule(0.1, () => this.history.ScrollToBottom());
+		});
+
+		$.RegisterForUnhandledEvent('PanoramaComponent_SteamLobby_OnLobbyStateChanged', () =>
+			$.Schedule(0.1, () => this.history.ScrollToBottom())
 		);
 	}
 
@@ -49,28 +63,8 @@ class ChatHandler {
 		}
 	}
 
-	onNewChatEntry(panel: GenericPanel) {
-		$.Schedule(0, () => panel.ScrollParentToMakePanelFit(0, false)); // IDK, ScrollToBottom always just scrolled to the second last msg
-		// Emote test for BLT to look into
-		// let messageLabel = panel.GetChild(0);
-		// let message = messageLabel.text;
-		// message = message.replace(/dogathan/g, "<img src='file://{images}/emotes/dogathan.png' height='16' width='16'/>");
-		// message = message.replace(/a/g, 'ä').replace(/o/g, 'ö').replace(/u/g, 'ü');
-		// messageLabel.text = message;
-
-		// TODO: Would be good to let you mute players from the specific chat message esp in large lobbies
-		// where it's hard to find them in the lobby list, but we need the steam id passed in somewhere
-
-		// panel.SetPanelEvent('oncontextmenu', () => {
-		// 	UiToolkitAPI.ShowSimpleContextMenu(panel, '', [
-		// 		{
-		// 			label: 'Mute Player',
-		// 			icon: 'file://{images}/volume-mute.svg',
-		// 			style: 'icon-color-red',
-		// 			jsCallback: () => ChatAPI.ChangeMuteState(needs steam id!!!, true)
-		// 		}
-		// 	]);
-		// });
+	onNewChatEntry(_panel: GenericPanel) {
+		this.history.ScrollToBottom();
 	}
 
 	onSteamLobbyMemberDataUpdated(memberData: MemberData) {
