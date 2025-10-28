39bd302dd8943990a52cbafe04c90a359b1820cb
tsa96 <tom@trumpetdust.org>
2025-10-28 15:07:46 +0000

    chore: update website dependencies

---
+++ scripts/common/web/consts/file-store-paths.const.ts
@@ -0,0 +1,41 @@
+import { FlatMapList } from '../enums/flat-map-list.enum';
+
+export function bspPath(key: string | number): string {
+	return `maps/${key}.bsp`;
+}
+
+export function vmfsPath(key: string | number): string {
+	return `maps/${key}_VMFs.zip`;
+}
+
+export function imgSmallPath(key: string): string {
+	return `img/${key}-small.jpg`;
+}
+
+export function imgMediumPath(key: string): string {
+	return `img/${key}-medium.jpg`;
+}
+
+export function imgLargePath(key: string): string {
+	return `img/${key}-large.jpg`;
+}
+
+export function imgXlPath(key: string): string {
+	return `img/${key}-xl.jpg`;
+}
+
+export function runPath(key: string | number | bigint): string {
+	return `runs/${key}`;
+}
+
+export function mapListPath(type: FlatMapList, version: string | number): string {
+	return `maplist/${type === FlatMapList.APPROVED ? 'approved' : 'submissions'}/${version}.dat`;
+}
+
+export function mapListDir(type: FlatMapList) {
+	return type === FlatMapList.APPROVED ? 'maplist/approved/' : 'maplist/submissions/';
+}
+
+export function mapReviewAssetPath(key: string): string {
+	return `mapreview/${key}`;
+}
---
+++ scripts/common/web/maps/role-names.map.ts
@@ -0,0 +1,12 @@
+import { Role } from '../enums/role.enum';
+
+export const RoleNames: ReadonlyMap<Role, string> = new Map([
+	[Role.ADMIN, 'Admin'],
+	[Role.MODERATOR, 'Moderator'],
+	[Role.MAPPER, 'Mapper'],
+	[Role.PORTER, 'Porter'],
+	[Role.DELETED, 'Deleted'],
+	[Role.PLACEHOLDER, 'Placeholder'],
+	[Role.REVIEWER, 'Reviewer'],
+	[Role.VERIFIED, 'Verified']
+]);
