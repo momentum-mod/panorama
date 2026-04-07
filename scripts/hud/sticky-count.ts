ab39e6051cd56c6e9c7f580552147da53a9a5e83
Natan Bębenek <natanb97@gmail.com>
2026-04-07 15:48:19 +0200

    WIP: add sticky count to customizer

--- scripts/hud/sticky-count.ts
+++ scripts/hud/sticky-count.ts
@@ -1,5 +1,8 @@
 import { PanelHandler } from 'util/module-helpers';
-import { registerHUDCustomizerComponent } from 'common/hud-customizer';
+import { RegisterHUDPanelForGamemode } from '../util/register-for-gamemodes';
+import { Gamemode } from 'common/web/enums/gamemode.enum';
+
+import { CustomizerPropertyType, registerHUDCustomizerComponent } from 'common/hud-customizer';
 
 export enum StickyState {
 	NOSTICKY = 0,
@@ -8,42 +11,126 @@ export enum StickyState {
 	BLOCKED = 3
 }
 
-// in order, according to state types
-const StickyPanelClasses = new Map([
-	[StickyState.NOSTICKY, 'sticky-panel--nosticky'],
-	[StickyState.ARMING, 'sticky-panel--sticky-arming'],
-	[StickyState.ARMED, 'sticky-panel--sticky-armed'],
-	[StickyState.BLOCKED, 'sticky-panel--sticky-blocked']
-]);
+//These values don't matter, defaults should be set through momentum/cfg/hud_default.kv3
+const Colors = {
+	BACKGROUND: 'rgba(35, 50, 57, 1)',
+	ARMING: 'rgba(35, 50, 57, 1)',
+	ARMED: 'gradient(linear, 0% 0%, 100% 0%, from (rgba(24, 150, 211, 1), to(rgba(113, 240, 255, 1)))',
+	BLOCKED: 'gradient(linear, 0% 0%, 100% 0%, from (rgba(134, 65, 65, 1), to(rgba(170, 65, 65, 1)))'
+};
 
 @PanelHandler()
 class StickyCountHandler {
+	readonly panels = {
+		countContainer: $('#StickyCountContainer')
+	};
+
 	constructor() {
-		$.RegisterEventHandler('OnStickyPanelStateChanged', $.GetContextPanel(), this.onStickyPanelStateChanged);
+		RegisterHUDPanelForGamemode({
+			gamemodes: [Gamemode.SJ],
+			events: [
+				{
+					event: 'OnStickyPanelStateChanged',
+					callback: (stickyPanel, state, prevstate) =>
+						this.onStickyPanelStateChanged(stickyPanel, state, prevstate)
+				}
+			]
+		});
 
 		registerHUDCustomizerComponent($.GetContextPanel(), {
 			resizeX: false,
-			resizeY: false
+			resizeY: false,
+			//TODO: Figure out how to initialize this
+			//TODO: Add box shadow setting
+			dynamicStyles: {
+				//TODO: Make first 3 options sliders?
+				width: {
+					name: 'Width',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.sticky-panel',
+					styleProperty: 'width',
+					valueFn: (value) => `${value}px`,
+					settingProps: { min: 1, max: 50 }
+				},
+				height: {
+					name: 'Height',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.sticky-panel',
+					styleProperty: 'height',
+					valueFn: (value) => `${value}px`,
+					settingProps: { min: 1, max: 50 }
+				},
+				borderRadius: {
+					name: 'Border Radius',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.sticky-panel',
+					styleProperty: 'borderRadius',
+					valueFn: (value) => `${value}%`,
+					settingProps: { min: 0, max: 50 }
+				},
+				gap: {
+					name: 'Gap',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.sticky-panel',
+					styleProperty: 'margin',
+					valueFn: (value) => `0px ${value}px 0px ${value}px`
+				},
+
+				//TODO: Consider allowing for a gradient
+				//TODO: Remove the stupid initialize hack
+				backgroundColor: {
+					name: 'Background Color',
+					type: CustomizerPropertyType.COLOR_PICKER,
+					callbackFunc: (_, value) => {
+						Colors.BACKGROUND = value;
+						this.stupidInitializeHack();
+					}
+				},
+				armingGradient: {
+					name: 'Arming Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					callbackFunc: (_, value) => {
+						Colors.ARMING = `gradient(linear, 0% 0%, 100% 0%, from (${value[0]}, to(${value[1]}))`;
+					}
+				},
+				armedGradient: {
+					name: 'Armed Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					callbackFunc: (_, value) => {
+						Colors.ARMED = `gradient(linear, 0% 0%, 100% 0%, from (${value[0]}, to(${value[1]}))`;
+					}
+				},
+				blockedGradient: {
+					name: 'Blocked Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					callbackFunc: (_, value) => {
+						Colors.BLOCKED = `gradient(linear, 0% 0%, 100% 0%, from (${value[0]}, to(${value[1]}))`;
+					}
+				}
+			}
+		});
+	}
+
+	stupidInitializeHack() {
+		const stickyPanels = this.panels.countContainer.Children();
+		stickyPanels.forEach((panel) => {
+			panel.style.backgroundColor = Colors.BACKGROUND as color;
 		});
 	}
 
 	onStickyPanelStateChanged(stickyPanel: Panel, state: StickyState, prevstate: StickyState) {
-		stickyPanel.AddClass(StickyPanelClasses.get(state));
 		switch (state) {
 			case StickyState.ARMED:
-				// keep arming class to have a smooth transition
-				if (prevstate !== StickyState.ARMING) stickyPanel.RemoveClass(StickyPanelClasses.get(prevstate));
+				stickyPanel.style.backgroundColor = Colors.ARMED as color;
 				break;
 			case StickyState.ARMING:
+				stickyPanel.style.backgroundColor = Colors.ARMING as color;
+				break;
 			case StickyState.BLOCKED:
-				stickyPanel.RemoveClass(StickyPanelClasses.get(prevstate));
+				stickyPanel.style.backgroundColor = Colors.BLOCKED as color;
 				break;
 			case StickyState.NOSTICKY:
-			default:
-				// remove all classes except the no sticky one
-				StickyPanelClasses.values()
-					.filter((spClass) => spClass !== StickyPanelClasses.get(StickyState.NOSTICKY))
-					.forEach((spClass) => stickyPanel.RemoveClass(spClass));
+				stickyPanel.style.backgroundColor = Colors.BACKGROUND as color;
 				break;
 		}
 	}
