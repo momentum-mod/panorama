a7f51fb39141840a22ca55916a197c498fbe85b3
Natan Bębenek <natanb97@gmail.com>
2026-04-07 22:01:19 +0200

    Add conc cooktime to customizer

--- scripts/hud/conc-cook-time.ts
+++ scripts/hud/conc-cook-time.ts
@@ -1,25 +1,92 @@
 import { PanelHandler } from 'util/module-helpers';
+import { RegisterHUDPanelForGamemode } from '../util/register-for-gamemodes';
+import { Gamemode } from 'common/web/enums/gamemode.enum';
+
+import { CustomizerPropertyType, registerHUDCustomizerComponent } from 'common/hud-customizer';
 
 @PanelHandler()
 class ConcCookHandler {
-	cp = $.GetContextPanel<MomHudConcCookTime>();
-	cookMeter = $<ProgressBar>('#ConcCookMeter');
-	cookLabel = $<Label>('#ConcCookTime');
-	secondStr = $.Localize('#Run_Stat_Unit_Second');
+	readonly panels = {
+		cp: $.GetContextPanel<MomHudConcCookTime>(),
+		cookMeter: $<ProgressBar>('#ConcCookMeter'),
+		cookLabel: $<Label>('#ConcCookTime')
+	};
 
-	onCookUpdate(time: float, percentage: float) {
-		this.cookMeter.value = percentage;
-
-		const labelEnabled = this.cp.concTimerLabelEnabled;
-		this.cookLabel.visible = labelEnabled;
-		if (labelEnabled) {
-			this.cookLabel.text = `${time.toFixed(2)}${this.secondStr}`;
-		}
-	}
+	units = $.Localize('#Run_Stat_Unit_Second');
+	isLabelEnabled: boolean;
 
 	constructor() {
-		$.RegisterEventHandler('OnCookUpdate', $('#ConcCooktimeContainer'), (time: float, percentage: float) =>
-			this.onCookUpdate(time, percentage)
-		);
+		RegisterHUDPanelForGamemode({
+			gamemodes: [Gamemode.CONC],
+			events: [
+				{
+					event: 'OnCookUpdate',
+					callback: (time, percentage) => this.onCookUpdate(time, percentage)
+				}
+			]
+		});
+
+		registerHUDCustomizerComponent($.GetContextPanel(), {
+			resizeX: true,
+			resizeY: false,
+			//TODO: Add entpanels
+			//TODO: Add box shadow setting
+			dynamicStyles: {
+				showLabel: {
+					name: 'Show Label',
+					type: CustomizerPropertyType.CHECKBOX,
+					callbackFunc: (_, value) => {
+						this.isLabelEnabled = value;
+						this.onCookUpdate(0, 0);
+					}
+				},
+				borderRadius: {
+					name: 'Border Radius',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.cooktime__meter',
+					styleProperty: 'borderRadius',
+					valueFn: (value) => `${value}px`,
+					settingProps: { min: 0, max: 10 }
+				},
+				fillRadius: {
+					name: 'Fill Radius',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '#ConcCookMeter_Left',
+					styleProperty: 'borderRadius',
+					valueFn: (value) => `${value}px`,
+					settingProps: { min: 0, max: 10 }
+				},
+				backgroundColor: {
+					name: 'Background Color',
+					type: CustomizerPropertyType.COLOR_PICKER,
+					targetPanel: '.cooktime__meter',
+					callbackFunc: (panel, value) => {
+						const progressBarRight = panel.GetLastChild();
+						panel.style.backgroundColor = value as color;
+						progressBarRight.style.backgroundColor = value as color;
+					}
+				},
+				fillGradient: {
+					name: 'Fill Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					targetPanel: '#ConcCookMeter_Left',
+					styleProperty: 'backgroundColor',
+					valueFn: (value) => {
+						return `gradient(linear, 0% 0%, 100% 0%, from (${value[0]}), to(${value[1]}))` as color;
+					}
+				},
+				fontColor: {
+					name: 'Font Color',
+					type: CustomizerPropertyType.COLOR_PICKER,
+					targetPanel: '.cooktime__label',
+					styleProperty: 'color'
+				}
+			}
+		});
+	}
+
+	onCookUpdate(time: float, percentage: float) {
+		this.panels.cookMeter.value = percentage;
+		this.panels.cookLabel.text = this.isLabelEnabled ? `${time.toFixed(2)}${this.units}` : '';
 	}
 }
