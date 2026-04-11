92dd38ed3beed12f015a437a83b4244b230211f6
Natan Bębenek <natanb97@gmail.com>
2026-04-11 12:31:01 +0200

    WIP: Add spectator hud to customizer
    Backbuffer issues need to be resolved to finish this

--- scripts/hud/spectate.ts
+++ scripts/hud/spectate.ts
@@ -1,5 +1,7 @@
 import { OnPanelLoad, PanelHandler } from 'util/module-helpers';
 
+import { registerHUDCustomizerComponent } from 'common/hud-customizer';
+
 @PanelHandler()
 class HudSpectateHandler implements OnPanelLoad {
 	readonly panels = {
@@ -15,6 +17,13 @@ class HudSpectateHandler implements OnPanelLoad {
 	constructor() {
 		$.RegisterForUnhandledEvent('ObserverTargetChanged', () => this.update());
 		$.RegisterForUnhandledEvent('MomentumSpectatorModeChanged', (newMode) => this.onSpectatorModeChange(newMode));
+
+		registerHUDCustomizerComponent($.GetContextPanel(), {
+			resizeX: false,
+			resizeY: false,
+			moveX: false,
+			moveY: false
+		});
 	}
 
 	onPanelLoad() {
