109310da646f80487fe72ea87735d4db6cf39b12
Natan Bębenek <natanb97@gmail.com>
2026-04-12 13:54:28 +0200

    Add DFJump indicator to customizer

--- scripts/hud/df-jump.ts
+++ scripts/hud/df-jump.ts
@@ -3,10 +3,13 @@ import { RegisterHUDPanelForGamemode } from 'util/register-for-gamemodes';
 import { GamemodeCategory } from 'common/web/enums/gamemode.enum';
 import { GamemodeCategories } from 'common/web/maps/gamemodes.map';
 
-enum ColorClass {
-	AIR = 'dfjump__press--air',
-	GROUND = 'dfjump__press--ground'
-}
+import { CustomizerPropertyType, registerHUDCustomizerComponent } from 'common/hud-customizer';
+
+//This gets overridden on map load. Set defaults in cfg/hud_default.kv3
+const Colors = {
+	AIR: 'gradient(linear, 30% 0%, 100% 0%, from(rgba(24, 150, 211, 1)), to(rgba(63, 74, 202, 1)))',
+	GROUND: 'gradient(linear, 30% 0%, 100% 0%, from(rgba(122, 238, 122, 1)), to(rgba(21, 152, 86, 1)))'
+};
 
 const DEFAULT_DELAY = 360;
 
@@ -21,7 +24,6 @@ class DFJumpHandler {
 		totalLabel: $<Label>('#JumpTotalLabel')
 	};
 
-	colorClass: ColorClass;
 	inverseMaxDelay: float;
 
 	constructor() {
@@ -43,23 +45,103 @@ class DFJumpHandler {
 				}
 			]
 		});
+
+		registerHUDCustomizerComponent($.GetContextPanel(), {
+			resizeX: false,
+			resizeY: false,
+			//TODO: Add border styles
+			dynamicStyles: {
+				showLabels: {
+					name: 'Show Labes',
+					type: CustomizerPropertyType.CHECKBOX,
+					targetPanel: '.dfjump__text-wrapper',
+					styleProperty: 'visibility',
+					valueFn: (value) => {
+						if (value) return 'visible';
+						else return 'collapse';
+					}
+				},
+				font: {
+					name: 'Font',
+					type: CustomizerPropertyType.FONT_PICKER,
+					targetPanel: [
+						'.dfjump__text-wrapper--release',
+						'.dfjump__text-wrapper--total',
+						'.dfjump__text-wrapper--press'
+					],
+					styleProperty: 'fontFamily'
+				},
+				fontSize: {
+					name: 'Font Size',
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: [
+						'.dfjump__text-wrapper--release',
+						'.dfjump__text-wrapper--total',
+						'.dfjump__text-wrapper--press'
+					],
+					styleProperty: 'fontSize',
+					valueFn: (value) => `${value}px`
+				},
+				fontColor: {
+					name: 'Font Color',
+					type: CustomizerPropertyType.COLOR_PICKER,
+					targetPanel: [
+						'.dfjump__text-wrapper--release',
+						'.dfjump__text-wrapper--total',
+						'.dfjump__text-wrapper--press'
+					],
+					styleProperty: 'color'
+				},
+				backgroundGradient: {
+					name: 'Background Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					targetPanel: ['.dfjump__release', '.dfjump__press'],
+					callbackFunc: (panel, value) => {
+						panel.GetLastChild().style.backgroundColor =
+							`gradient(linear, 0% 0%, 100% 0%, from(${value[0]}), to(${value[1]}))` as color;
+					}
+				},
+				airGradient: {
+					name: 'Air Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					callbackFunc: (_, value) => {
+						Colors.AIR = `gradient(linear, 30% 0%, 100% 0%, from(${value[0]}), to(${value[1]}))`;
+						this.onDFJumpUpdate(0, 0, 0);
+					}
+				},
+				releaseGradient: {
+					name: 'Release Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					targetPanel: '.dfjump__release',
+					callbackFunc: (panel, value) => {
+						panel.GetFirstChild().style.backgroundColor =
+							`gradient(linear, 30% 0%, 100% 0%, from(${value[0]}), to(${value[1]}))` as color;
+					}
+				},
+				groundGradient: {
+					name: 'Ground Gradient',
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					callbackFunc: (_, value) => {
+						Colors.GROUND = `gradient(linear, 30% 0%, 100% 0%, from(${value[0]}), to(${value[1]}))`;
+						this.onDFJumpUpdate(0, 0, 0);
+					}
+				}
+			}
+		});
 	}
 
 	onMapInit() {
 		this.initializeSettings();
-		this.colorClass = ColorClass.GROUND;
 	}
 
 	onDFJumpUpdate(releaseDelay: float, pressDelay: float, totalDelay: float) {
 		const releaseRatio = releaseDelay * this.inverseMaxDelay;
 		const pressRatio = Math.abs(pressDelay) * this.inverseMaxDelay;
-		const newPressColorClass = pressDelay < 0 ? ColorClass.GROUND : ColorClass.AIR;
+		const newPressColor = pressDelay < 0 ? Colors.GROUND : Colors.AIR;
 
 		this.panels.releaseBar.value = releaseRatio;
 		this.panels.pressBar.value = pressRatio;
-		this.panels.pressBar.RemoveClass(this.colorClass);
-		this.panels.pressBar.AddClass(newPressColorClass);
-		this.colorClass = newPressColorClass;
+		this.panels.pressBar.GetFirstChild().style.backgroundColor = newPressColor as color;
 
 		this.panels.releaseLabel.text = releaseDelay.toFixed(0);
 		this.panels.pressLabel.text = pressDelay.toFixed(0);
