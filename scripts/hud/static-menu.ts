91d7418324e773e2e56af1af4ee4a3a841da8f2d
Natan Bębenek <natanb97@gmail.com>
2026-04-22 13:46:49 +0200

    WIP: Add static menu to customizer

--- scripts/hud/static-menu.ts
+++ scripts/hud/static-menu.ts
@@ -1,3 +1,17 @@
-$.RegisterEventHandler('StaticHudMenu_EntrySelected', $.GetContextPanel(), (panel: Panel) => {
-	panel.TriggerClass('static-hud-menu__entry--highlight');
-});
+import { PanelHandler } from 'util/module-helpers';
+
+import { registerHUDCustomizerComponent } from 'common/hud-customizer';
+
+@PanelHandler()
+class HudMapInfoHandler {
+	constructor() {
+		$.RegisterEventHandler('StaticHudMenu_EntrySelected', $.GetContextPanel(), (panel: Panel) => {
+			panel.TriggerClass('static-hud-menu__entry--highlight');
+		});
+
+		registerHUDCustomizerComponent($.GetContextPanel(), {
+			resizeX: false,
+			resizeY: false
+		});
+	}
+}
